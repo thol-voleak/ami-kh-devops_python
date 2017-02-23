18df77d0c71fa29d1f23edad144d4ae8928da44f
wutcharin munjai <wutcharin.mun@ascendcorp.com>
2017-02-23 11:58:03 +0700

    [EQTR-88] Separate configuration global_setting.py

--- web_admin/web_admin/settings.py
+++ web_admin/web_admin/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import sys
 
 BASE_URL = 'https://abc.com'
 
@@ -97,12 +98,10 @@ WSGI_APPLICATION = 'web_admin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# Load configuration from configuration file
+sys.path.append('/data/projects/ami-admin-portal/config')
+
+from global_settings import *
 
 LOG_DIR = '/data/logs/ami-admin-portal'
 LOG_FILENAME = os.path.join(LOG_DIR, 'ami-admin-portal.log')
