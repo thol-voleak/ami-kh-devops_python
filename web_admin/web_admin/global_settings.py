8917168e5f0514179bdccb722d818481e36beea5
Ngo Lien <ngo.hoang.lien@gmail.com>
2017-02-23 17:17:02 +0700

    [EQTR-90] - Change “Look and Feel” of Login Form

--- web_admin/web_admin/global_settings.py
+++ web_admin/web_admin/global_settings.py
@@ -4,3 +4,7 @@ DATABASES = {
         'NAME': 'db.sqlite3',
     }
 }
+
+CLIENTID = "A1234567890123456789012345678901"
+CLIENTSECRET = "A123456789012345678901234567890123456789012345678901234567890123"
+LOGIN_URL = 'http://alp-eq-esg-01.tmn-dev.com/api-gateway/system-user/v1/oauth/token'
