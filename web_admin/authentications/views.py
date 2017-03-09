486b8017bbe5145e8ad610e616180786a91d3184
wutcharin munjai <wutcharin.mun@ascendcorp.com>
2017-03-09 11:23:47 +0700

    Fixes logout error - reviewed by Alex

--- web_admin/authentications/views.py
+++ web_admin/authentications/views.py
@@ -9,10 +9,14 @@ logger = logging.getLogger(__name__)
 def logout_user(request):
     logger.info("{} username start logging out".format(request.user))
     if request.user.is_authenticated:
-        auth = Authentications.objects.get(user=request.user)
-        if auth is not None:
-            logger.info('Deleting current session info')
-            auth.delete()
+        try:
+            auth = Authentications.objects.get(user=request.user)
+            if auth is not None:
+                logger.info('Deleting current session info')
+                auth.delete()
+        except Exception as e:
+            logger.error("Access token not found for {} username".format(request.user))
+            pass
 
     logout(request)
     logger.info("{} username was logout".format(request.user))
