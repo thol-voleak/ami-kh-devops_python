0092455c4bd8980db735e2448235dd5f0fd8ba40
wutcharin munjai <wutcharin.mun@ascendcorp.com>
2017-03-13 17:02:43 +0700

    fixes error on list page

--- web_admin/clients/views/list.py
+++ web_admin/clients/views/list.py
@@ -15,23 +15,9 @@ class ListView(TemplateView):
     def get_context_data(self, **kwargs):
         logger.info('========== Start get Clients List ==========')
         data = self.get_clients_list()
-        refined_data = self.refine_data(data)
+        refined_data = _refine_data(data)
         logger.info('========== Finished get Clients List ==========')
         return {'data': refined_data}
-
-    @staticmethod
-    def refine_data(self, clients_list):
-        for client in clients_list:
-            if (client['created_timestamp'] is not None) and (client['created_timestamp'] != "null"):
-                created_at = client['created_timestamp'] / 1000.0
-                client['created_timestamp'] = datetime.datetime.fromtimestamp(float(created_at)).strftime(
-                    '%d-%m-%Y %H:%M %p')
-
-            if (client['last_updated_timestamp'] is not None) and (client['last_updated_timestamp'] != "null"):
-                created_at = client['last_updated_timestamp'] / 1000.0
-                client['last_updated_timestamp'] = datetime.datetime.fromtimestamp(float(created_at)).strftime(
-                    '%d-%m-%Y %H:%M %p')
-        return clients_list
 
     def get_clients_list(self):
         client_id = settings.CLIENTID
@@ -48,18 +34,31 @@ class ListView(TemplateView):
             'correlation-id': correlation_id,
             'client_id': client_id,
             'client_secret': client_secret,
-            'Authorization': 'Bearer ' + access_token,
+            'Authorization': access_token,
         }
 
         logger.info('Getting client list from backend')
         auth_request = requests.get(url, headers=headers, verify=False)
-        logger.info("Received data with response {}".format(auth_request.content))
+        logger.info("Received data with response is {}".format(auth_request.status_code))
 
         json_data = auth_request.json()
         data = json_data.get('data')
-
         if auth_request.status_code == 200:
             if (data is not None) and (len(data) > 0):
                 return data
 
         raise Exception("{}".format(json_data["message"]))
+
+
+def _refine_data(clients_list):
+    for client in clients_list:
+        if (client['created_timestamp'] is not None) and (client['created_timestamp'] != "null"):
+            created_at = client['created_timestamp'] / 1000.0
+            client['created_timestamp'] = datetime.datetime.fromtimestamp(float(created_at)).strftime(
+                '%d-%m-%Y %H:%M %p')
+
+        if (client['last_updated_timestamp'] is not None) and (client['last_updated_timestamp'] != "null"):
+            created_at = client['last_updated_timestamp'] / 1000.0
+            client['last_updated_timestamp'] = datetime.datetime.fromtimestamp(float(created_at)).strftime(
+                '%d-%m-%Y %H:%M %p')
+    return clients_list
