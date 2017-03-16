65ede51ecf92f925347f3b3c45586e4e08a160ec
Nonthanun Sudlapa <nonthanun.sud@ascendcorp.com>
2017-03-16 17:47:19 +0700

    [EQTR-95] add application log for regenerate client secret [Review by P'Klang]

--- web_admin/clients/api.py
+++ web_admin/clients/api.py
@@ -36,7 +36,8 @@ class ClientApi():
         start_date = time.time()
         response = requests.post(url, headers=headers, verify=False)
         done = time.time()
-        logger.info("Response time for regenerate client secret is {} sec.".format(done - start_date))
+        logger.info("Response time for regenerate client secret for {} client id is {} sec.".format(client_id, done - start_date))
+        logger.info("Response for regenerate client secret for {} client id is {}".format(client_id, response.content))
 
         response_json = response.json()
 
