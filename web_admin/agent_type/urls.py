6ec4b5fd69eb58e230c4153148ca3a1e7b5a6317
Alex - lien.ngo <lien.ngo@ascendcorp.com>
2017-03-27 11:18:50 +0700

    [EQTR-156] - Delete Agent Type - Reviewed by @Gate

--- web_admin/agent_type/urls.py
+++ web_admin/agent_type/urls.py
@@ -1,12 +1,12 @@
-
-
 from . import views
 from .views.create import AgentTypeCreate
 from .views.detail import DetailView
-
+from .views.delete import DeleteView
 from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
 from .views.list import ListView
+from .views.delete import delete_agent_type
+# from .api import ClientApi
 
 app_name = 'agent_type'
 
@@ -17,7 +17,9 @@ urlpatterns = [
     url(r'^list/$', login_required(ListView.as_view(), login_url='login'), name="agent-type-list"),
     url(r'^detail/(?P<agentTypeId>[0-9A-Za-z]+)/$', login_required(DetailView.as_view(), login_url='login'),
         name="agent-type-detail"),
+    url(r'^delete/(?P<agent_type_id>[0-9A-Za-z]+)/$', login_required(DeleteView.as_view(), login_url='login'),
+        name="agent-type-delete"),
+    url(r'^(?P<agent_type_id>[0-9A-Za-z]+)/delete/$', login_required(delete_agent_type, login_url='login'),
+        name="delete-agent-type"),
 
-    ]
-
-
+]
