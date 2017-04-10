9aa2cf1bf635da3909569bf9df09ab850a278ea8
Manh Tai <manhtai@ip-192-168-0-36.ap-southeast-1.compute.internal>
2017-04-10 14:25:15 +0700

    [EQTR-640] Add scopes to client detail page

--- web_admin/clients/views/detail.py
+++ web_admin/clients/views/detail.py
@@ -1,70 +1,53 @@
 import logging
-import random
-import string
-import time
-import requests
 
+import requests
 from django.conf import settings
 from django.views.generic.base import TemplateView
-from authentications.apps import InvalidAccessToken
 
-from authentications.models import *
+from web_admin.mixins import GetChoicesMixin
 
 logger = logging.getLogger(__name__)
 
 
-class DetailView(TemplateView):
+class DetailView(TemplateView, GetChoicesMixin):
     template_name = "clients/client_detail.html"
 
     def get_context_data(self, **kwargs):
-        try:
-            logger.info('========== Start getting client detail ==========')
+        context = super(DetailView, self).get_context_data(**kwargs)
+        client_id = context['client_id']
 
-            context = super(DetailView, self).get_context_data(**kwargs)
-            client_id = context['client_id']
-            logger.info('========== Finished getting client detail ==========')
+        logger.info('========== Start getting client detail ==========')
+        client_info, success_info = self._get_client_detail(client_id)
+        logger.info('========== Finished getting client detail ==========')
 
-            return self._get_client_detail(client_id)
-        except:
-            context = {'client_info': {},
-                       'error_msg': 'Sorry, we cannot get client detail.'}
-            return context
+        logger.info('========== Start getting client scopes ==========')
+        client_scopes, success_scope = self._get_client_scopes(client_id)
+        logger.info('========== Finished getting client scopes ==========')
+
+        context['client_info'] = client_info
+        context['client_scopes'] = client_scopes
+        return context
+
+    def _get_client_scopes(self, client_id):
+        url = settings.CLIENT_SCOPES.format(client_id=client_id)
+        response = requests.get(url, headers=self._get_headers(), verify=False)
+        logger.info("Get client scopes url: {}".format(url))
+        logger.info("Received data with response status: {}".format(response.status_code))
+
+        if response.status_code == 200:
+            logger.info("Client scopes was fetched.")
+            response_json = response.json()
+            return response_json.get('data'), True
+        return None, False
 
     def _get_client_detail(self, client_id):
-
         url = settings.CLIENTS_LIST_URL + '/' + client_id
-        correlation_id = ''.join(
-            random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10))
-
-        try:
-            auth = Authentications.objects.get(user=self.request.user)
-            access_token = auth.access_token
-        except Exception as e:
-            raise InvalidAccessToken("{}".format(e))
-
-        headers = {
-            'content-type': 'application/json',
-            'correlation-id': correlation_id,
-            'client_id': settings.CLIENTID,
-            'client_secret': settings.CLIENTSECRET,
-            'Authorization': 'Bearer ' + access_token,
-        }
-
-        logger.info('Getting client detail from backend')
-        start_date = time.time()
-        response = requests.get(url, headers=headers, verify=False)
+        response = requests.get(url, headers=self._get_headers(), verify=False)
         logger.info("Get client url: {}".format(url))
-        done = time.time()
-        logger.info("Response time is {} sec.".format(done - start_date))
-        logger.info("Received data with response status is {}".format(response.status_code))
+        logger.info("Received data with response status: {}".format(response.status_code))
 
-        response_json = response.json()
-        if response_json['status']['code'] == "success":
+        if response.status_code == 200:
             logger.info("Client detail was fetched.")
-            data = response_json.get('data')
-            context = {'client_info': data,
-                       'error_msg': None}
-            return context
-
-        if response_json["status"]["message"] == "Invalid access token":
-            raise InvalidAccessToken(response_json["status"]["message"])
+            response_json = response.json()
+            return response_json.get('data'), True
+        return None, False
