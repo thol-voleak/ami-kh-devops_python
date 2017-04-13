a77e5c1fd793a8c6732afe87d6e29128d8d5cda8
Steve Le <letronghiep1985@gmail.com>
2017-04-13 16:22:11 +0700

    [EQTR-594] - Add more agent id param for logging.

--- web_admin/balances/views/company.py
+++ web_admin/balances/views/company.py
@@ -68,7 +68,10 @@ class CompanyBalanceView(TemplateView, GetChoicesMixin):
 
     def _get_total_initial_company_balance(self, currency):
         logger.info("Getting total initial balance by username: {}".format(self.request.user.username))
-        logger.info("Getting total initial balance by agent id and currency: {}".format(currency))
+        logger.info("Getting total initial balance by agent id: {} and currency: {}".format(
+            self.company_agent_id,
+            currency,
+        ))
         url = settings.GET_AGENT_BALANCE_BY_CURRENCY.format(agent_id=self.company_agent_id, currency=currency)
         logger.info("Request url: {}".format(url))
 
