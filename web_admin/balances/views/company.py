75c4bdbd5436c8f5dd8ab843978e7982fc42162b
Steve Le <letronghiep1985@gmail.com>
2017-04-13 15:22:45 +0700

    [EQTR-594] - Correct format of logging to be more meaningful. (Time = 2)

--- web_admin/balances/views/company.py
+++ web_admin/balances/views/company.py
@@ -67,8 +67,7 @@ class CompanyBalanceView(TemplateView, GetChoicesMixin):
         return redirect(request.META['HTTP_REFERER'])
 
     def _get_total_initial_company_balance(self, currency):
-        logger.info("Getting total initial balance by user and currency: {}".format(self.request.user.username),
-                    currency)
+        logger.info("Getting total initial balance by agent id and currency: {}".format(currency))
         url = settings.GET_AGENT_BALANCE_BY_CURRENCY.format(agent_id=self.company_agent_id, currency=currency)
         logger.info("Request url: {}".format(url))
 
