869f4ee90b8b9b248e67d5918a98589f6f492159
Manh Tai <manhtai@ip-192-168-0-36.ap-southeast-1.compute.internal>
2017-04-26 16:10:24 +0700

    [EQTR-728] Add name to fee tier page [Reviewed by Klang]

--- web_admin/services/views/fee_tier.py
+++ web_admin/services/views/fee_tier.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 import requests
 from django.conf import settings
@@ -18,6 +19,7 @@ class FeeTierListView(TemplateView, GetHeaderMixin):
     def get_context_data(self, *args, **kwargs):
         context = super(FeeTierListView, self).get_context_data(*args, **kwargs)
         service_id = kwargs.get('service_id')
+        command_id = kwargs.get('command_id')
         service_command_id = kwargs.get('service_command_id')
         if not service_id or not service_command_id:
             raise Http404
@@ -32,6 +34,15 @@ class FeeTierListView(TemplateView, GetHeaderMixin):
         context['data'] = data
         context['msg'] = self.request.session.pop('add_tier_msg', None)
         context['edit_msg'] = self.request.session.pop('edit_tier_msg', None)
+
+        logger.info('========== Start get service name ==========')
+        context['service_name'] = self._get_service_name_by_id(service_id)
+        logger.info('========== Finish get service name ==========')
+
+        logger.info('========== Start get command name ==========')
+        context['command_name'] = self._get_command_name_by_id(command_id)
+        logger.info('========== Finish get command name ==========')
+
         return context
 
     def _get_fee_tier_list(self, service_command_id):
@@ -51,3 +62,41 @@ class FeeTierListView(TemplateView, GetHeaderMixin):
         else:
             logger.info('Response content: {}'.format(response.content))
             return [], False
+
+    def _get_command_name_by_id(self, command_id):
+        url = settings.COMMAND_LIST_URL
+        logger.info('URL: {}'.format(url))
+        start_date = time.time()
+        response = requests.get(url, headers=self._get_headers(),
+                                verify=settings.CERT)
+        done = time.time()
+        logger.info('Reponse time: {} sec.'.format(done - start_date))
+        logger.info('Response code: {}'.format(response.status_code))
+        logger.info('Response content: {}'.format(response.content))
+
+        json_response = response.json()
+        if response.status_code == 200 and json_response['status']['code'] == 'success':
+            data = json_response['data']
+            command_name =  [d['command_name']
+                             for d in data
+                             if str(d['command_id']) == str(command_id)]
+            if command_name:
+                return command_name[0]
+        return command_id
+
+    def _get_service_name_by_id(self, service_id):
+        url = settings.SERVICE_DETAIL_URL.format(service_id)
+        logger.info('URL: {}'.format(url))
+        start_date = time.time()
+        response = requests.get(url, headers=self._get_headers(),
+                                verify=settings.CERT)
+        done = time.time()
+        logger.info('Reponse time: {} sec.'.format(done - start_date))
+        logger.info('Response code: {}'.format(response.status_code))
+        logger.info('Response content: {}'.format(response.content))
+
+        json_response = response.json()
+        if response.status_code == 200 and json_response['status']['code'] == 'success':
+            data = json_response['data']
+            return data['service_name']
+        return service_id
