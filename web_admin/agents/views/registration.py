c8d24ea357168955e1032230714e8a0e77af35e3
Steve Le <letronghiep1985@gmail.com>
2017-05-05 14:14:35 +0700

    [EQTR-806] - Added comments in code and format the logging. Agent ID can return from API Server.

--- web_admin/agents/views/registration.py
+++ web_admin/agents/views/registration.py
@@ -14,7 +14,13 @@ import time
 
 logger = logging.getLogger(__name__)
 
-
+'''
+Author: Leo
+History:
+- 2017-05-04: ... (Leo)
+-- API 1: Load Agent Type       - GET api-gateway/agent/v1/types
+-- API 2: Load Currency List    - GET api-gateway/centralize-configuration/v1/scopes/global/currencies
+'''
 class AgentTypeAndPreloadCurrenciesDropDownList(TemplateView):
 
     def _get_agent_types_list(self):
@@ -61,51 +67,66 @@ class AgentTypeAndPreloadCurrenciesDropDownList(TemplateView):
         else:
             raise Exception("{}".format(json_data["status"]["message"]))
 
-
+'''
+Author: Steve Le
+History:
+- 2017-05-03: Initialize empty Class with template HTML name (Steve Le)
+- 2017-05-04: Add logic for Agent registration (Gate Nguyen)
+-- API 1: POST api-gateway/agent/v1/agents/{agent_id}/profiles [METHOD = _create_agent_profile]
+-- API 2: POST ...
+-- API 3: POST ...
+- 2017-05-05: Corrected API Logic make Agent registration work well (Steve Le)
+-- Added logging format and more.
+'''
 class AgentRegistration(GetChoicesMixin, AgentTypeAndPreloadCurrenciesDropDownList):
     template_name = "registration.html"
 
-
     def get_context_data(self, *arg, **kwargs):
+
+        # Get API that inherits from parent Class
         logger.info('========== Start get Currency List ==========')
         preload_currencies = self._get_preload_currencies_dropdown()
         logger.info('========== Finished get Currency List ==========')
 
         logger.info('========== Start get Agent Types List =========')
         agent_types_list = self._get_agent_types_list()
-
-        #print('----------------------------------------- {}'.format(agent_types_list))
         logging.info('========= Finish get Agent Types List =========')
 
-        result = {'preload_currencies': preload_currencies,
-                  'agent_types_list': agent_types_list,
-                  'msg': self.request.session.pop('agent_registration_msg', None)}
-        return result
+        result = {
+            'preload_currencies': preload_currencies,
+            'agent_types_list': agent_types_list,
+            'msg': self.request.session.pop('agent_registration_msg', None)
+        }
 
+        return result
 
     def post(self, request, *args, **kwargs):
         logger.info('========== Start Registering Agent Profile ==========')
-        data = self._create_agent_profile(request)
+        agent_profile_reponse = self._create_agent_profile(request)
         logger.info('========== Finished Registering Agent Profile ==========')
 
-        agent_id = 1 # data['id']
+        if agent_profile_reponse is not None:
+            agent_id = agent_profile_reponse['id']
 
         logger.info('========== Start create agent identity ==========')
         self._create_agent_identity(request, agent_id)
         logger.info('========== Finished create agent identity ==========')
 
         logger.info('========== Start create agent balance ==========')
-        success = self._create_agent_balance(request, agent_id)
+        self._create_agent_balance(request, agent_id)
         logger.info('========== Finished create agent balance ==========')
 
         request.session['agent_registration_msg'] = 'Registering Agent successfully'
+
         return redirect('agents:agent_detail', agent_id=agent_id)
 
     def _create_agent_profile(self, request):
+
+        # Prepare for agent registration.
         password = request.POST.get('password')
         agent_type_id = request.POST.get('agent_type_id')
         parent_id = request.POST.get('parent_id')
-        grand_parent_id = 3  # request.POST.get('grand_parent_id')
+        grand_parent_id = 3  # request.POST.get('grand_parent_id')  # TODO: Hard code for waiting new API
         bank_name =  request.POST.get('bank_name')
         agent_bank_account =  request.POST.get('agent_bank_account')
         card_id = request.POST.get('card_id')
@@ -166,80 +187,80 @@ class AgentRegistration(GetChoicesMixin, AgentTypeAndPreloadCurrenciesDropDownLi
             'kyc_status': kyc_status,
             'status': status,
         }
-        logger.info("User: {}".format(self.request.user.username))
 
-        url = settings.DOMAIN_NAMES + settings.AGENT_REGISTRATION_URL
+        api_path = settings.AGENT_REGISTRATION_URL
+        url = settings.DOMAIN_NAMES + api_path
 
-        logger.info('Request url: {}'.format(url))
-        logger.info('Request body: {}'.format(body))
+        logger.info('API-Path: {}'.format(api_path))
+        logger.info('Params: {}'.format(body))
+
         start_time = time.time()
-
         response = requests.post(url, headers=self._get_headers(), json=body, verify=settings.CERT)
         end_time = time.time()
 
-        logger.info("Response time: {} sec.".format(end_time - start_time))
-        logger.info("Received response with status {}".format(response.status_code))
-        logger.info("Response content is {}".format(response.content))
+        logger.info("Response_code: {}".format(response.status_code))
+        logger.info("Response_content: {}".format(response.content))
+        logger.info("Response_time: {} sec.".format(end_time - start_time))
 
         json_data = response.json()
         if response.status_code == 200:
             return json_data.get('data')
         else:
-            request.session['agent_registration_msg'] = 'Something wrong happened!'
+            request.session['agent_registration_msg'] = 'Agent registration - profile: something wrong happened!'
             return redirect('agents:agent_registration')
 
     def _create_agent_identity(self, request, agent_id):
 
         username = request.POST.get('username')
         password = request.POST.get('password')
+
         body = {
             'username': username,
             'password': password,
         }
 
-        logger.info("User: {}".format(self.request.user.username))
+        api_path = settings.CREATE_AGENT_IDENTITY_URL.format(agent_id=agent_id)
+        url = settings.DOMAIN_NAMES + api_path
 
-        url = settings.DOMAIN_NAMES + settings.CREATE_AGENT_IDENTITY_URL.format(agent_id=agent_id)
+        logger.info('API-Path: {}'.format(api_path))
+        logger.info('Params: {}'.format(body))
 
-        logger.info('Request url: {}'.format(url))
-        logger.info('Request body: {}'.format(body))
-
+        start_time = time.time()
         response = requests.post(url, headers=self._get_headers(), json=body, verify=settings.CERT)
+        end_time = time.time()
 
-        logger.info("Received response with status {}".format(response.status_code))
-        logger.info("Response content is {}".format(response.content))
+        logger.info("Response_code: {}".format(response.status_code))
+        logger.info("Response_content: {}".format(response.content))
+        logger.info("Response_time: {} sec.".format(end_time - start_time))
 
         if response.status_code == 200:
             return True
         else:
-            request.session['agent_registration_msg'] = 'Something wrong happened!'
+            request.session['agent_registration_msg'] = 'Agent registration - identity: Something wrong happened!'
             return redirect('agents:agent_registration')
 
     def _create_agent_balance(self, request, agent_id):
 
         currency = request.POST.get('currency')
-        sof_type = "cash"  # hardcode
+        sof_type = "cash"  # TODO: Hard code for Sof_Type
         body = {}
 
-        logger.info("User: {}".format(self.request.user.username))
+        api_path = settings.CREATE_AGENT_BALANCE_URL.format(agent_id=agent_id, sof_type=sof_type, currency=currency)
+        url = settings.DOMAIN_NAMES + api_path
 
-        url = settings.DOMAIN_NAMES + settings.CREATE_AGENT_BALANCE_URL.format(agent_id=agent_id, sof_type=sof_type,
-                                                                               currency=currency)
+        logger.info('API-Path: {}'.format(api_path))
+        logger.info('Params: {}'.format(body))
 
-        logger.info('Request url: {}'.format(url))
-        logger.info('Request body: {}'.format(body))
-
+        start_time = time.time()
         response = requests.post(url, headers=self._get_headers(), json=body, verify=settings.CERT)
+        end_time = time.time()
 
-        logger.info("Received response with status {}".format(response.status_code))
-        logger.info("Response content is {}".format(response.content))
+        logger.info("Response_code: {}".format(response.status_code))
+        logger.info("Response_content: {}".format(response.content))
+        logger.info("Response_time: {} sec.".format(end_time - start_time))
 
         if response.status_code == 200:
             return True
         else:
-            request.session['agent_registration_msg'] = 'Something wrong happened!'
+            request.session['agent_registration_msg'] = 'Agent registration - balance: Something wrong happened!'
             return redirect('agents:agent_registration')
-
-
-
-
