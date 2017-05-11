737eabd00bbd0f857722d1f9ae2bb6fdd04c4fed
nam_pham_vn <nam.pham@ascendcorp.com>
2017-05-11 10:50:22 +0700

    [EQTR-489] System user can see customer deatail - [reviewed by Alex]

--- web_admin/customers/urls.py
+++ web_admin/customers/urls.py
@@ -1,4 +1,5 @@
 from .views.list import ListView
+from .views.customer_detail import CustomerDetailView
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
@@ -6,4 +7,5 @@ app_name = 'customers'
 
 urlpatterns = [
     url(r'^$', login_required(ListView.as_view(), login_url='login'), name="customer-list"),
+    url(r'^(?P<customerId>[0-9A-Za-z]+)/$', login_required(CustomerDetailView.as_view(), login_url='login'), name="customer_detail"),
 ]
