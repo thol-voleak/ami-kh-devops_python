696da50b58882f9971305caa95b072f0fbae76e2
wutcharin munjai <wutcharin.mun@ascendcorp.com>
2017-05-23 14:07:25 +0700

    [EQTR-1125] Move api endpoint into code (cash sofs app)

--- web_admin/cash_sofs/views/cash_transaction_list.py
+++ web_admin/cash_sofs/views/cash_transaction_list.py
@@ -1,13 +1,14 @@
-import logging
-import time
+from web_admin.api_settings import CASH_TRANSACTIONS_URL
 
-import requests
 from django.conf import settings
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-
 from authentications.utils import get_auth_header
 from authentications.apps import InvalidAccessToken
+
+import requests
+import logging
+import time
 
 logger = logging.getLogger(__name__)
 
@@ -55,30 +56,34 @@ class CashTransactionView(TemplateView):
         return render(request, self.template_name, context)
 
     def get_cash_transaction_list(self, body):
-        url = settings.DOMAIN_NAMES + "api-gateway/report/v1/cash/transactions"
-
-        logger.info('Call search cash source of fund API to backend service. API-Path: {}'.format(url))
+        logger.info(
+            'Call search cash source of fund API to backend service. API-Path: {}'.format(CASH_TRANSACTIONS_URL))
         start = time.time()
-        logger.info("Request body: {};".format(body))
-        auth_request = requests.post(url, headers=get_auth_header(self.request.user), json=body, verify=settings.CERT)
+        logger.info("Request body: {}".format(body))
+        responses = requests.post(CASH_TRANSACTIONS_URL,
+                                  headers=get_auth_header(self.request.user),
+                                  json=body,
+                                  verify=settings.CERT)
         end = time.time()
-        logger.info("Response_code: {};".format(auth_request.status_code))
+        logger.info("Response_code: {}".format(responses.status_code))
         logger.info("Response_time: {} seconds".format(end - start))
 
-        json_data = auth_request.json()
+        json_data = responses.json()
         status = json_data.get('status', {})
         code = status.get('code', '')
-        if (code == "access_token_expire") or (code== 'access_token_not_found'):
-            message = status.get('message', 'Something went wrong.')
-            raise InvalidAccessToken(message)
-        data = json_data.get('data')
-        if auth_request.status_code == 200:
+
+        if responses.status_code == 200:
+            data = json_data.get('data')
             if data is not None and len(data) > 0:
                 logger.info('Cash transaction found {} records'.format(len(data)))
                 return data
         else:
-            logger.info('Response_content: {}'.format(auth_request.content))
-            return []
+            if (code == "access_token_expire") or (code == 'access_token_not_found'):
+                message = status.get('message', 'Something went wrong.')
+                raise InvalidAccessToken(message)
+
+            logger.info('Response_content: {}'.format(responses.content))
+            raise Exception(responses.content)
 
     def format_data(self, data):
         for i in data:
