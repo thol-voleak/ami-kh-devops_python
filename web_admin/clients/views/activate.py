1155a27134c3451c7ce9a8e0a2a37f82716a25f1
trananhkma <trananhkma@gmail.com>
2017-05-26 10:19:41 +0700

    Undo Refactoring code in active client

--- web_admin/clients/views/activate.py
+++ web_admin/clients/views/activate.py
@@ -1,6 +1,12 @@
-from django.http import HttpResponse
+import requests
+import time
+import copy
+
+from django.conf import settings
 from web_admin import api_settings
-from web_admin.restful_methods import RESTfulMethods
+from django.http import HttpResponse
+from authentications.apps import InvalidAccessToken
+from authentications.utils import get_auth_header
 
 import logging
 
@@ -8,12 +14,45 @@ logger = logging.getLogger(__name__)
 
 
 def activate(request, client_id):
-    url = api_settings.ACTIVATE_CLIENT_URL.format(client_id)
+    logger.info('========== Start activating client ==========')
+    logger.info('The Client to be activated {} client id.'.format(client_id))
+
     params = {
         'status': 'active',
     }
 
-    data, success = RESTfulMethods._put_method(url, "client", logger, params)
-    if success:
-        return HttpResponse(status=200, content=data)
-    return HttpResponse(status=500, content=data)
+    try:
+        url = settings.DOMAIN_NAMES + api_settings.ACTIVATE_CLIENT_URL.format(client_id)
+        data_log = copy.deepcopy(params)
+        data_log['client_secret'] = ''
+        logger.info("Expected client status {}".format(data_log))
+
+        start_date = time.time()
+        response = requests.put(url, headers=get_auth_header(request.user),
+                                json=params, verify=settings.CERT)
+        done = time.time()
+        logger.info("Response time is {} sec.".format(done - start_date))
+
+        response_json = response.json()
+        status = response_json['status']
+
+        logger.info("Response Code is {}".format(status['code']))
+
+        if response.status_code == 200:
+            if status['code'] == "success":
+                logger.info("Client was activated.")
+                logger.info('========== Finish activating client ==========')
+                return HttpResponse(status=200, content=response)
+            else:
+                logger.info("Error activating Client {}".format(client_id))
+                logger.info('========== Finish activating client ==========')
+                return HttpResponse(status=500, content=response)
+
+        if response_json["status"]["message"] == "Invalid access token":
+            raise InvalidAccessToken(response_json["status"]["message"])
+
+    except Exception as e:
+        logger.info(e)
+        logger.info('client_id: ' + client_id)
+        logger.info('========== Finish activating client ==========')
+        return HttpResponse(status=500, content=e)
