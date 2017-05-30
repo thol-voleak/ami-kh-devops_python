2abadf98dfca69500f4103c9df3686f7d2335278
wutcharin munjai <wutcharin.mun@ascendcorp.com>
2017-05-30 15:38:08 +0700

    [EQTR-1250] Add create page for create bank profile

--- web_admin/bank_sofs/urls.py
+++ web_admin/bank_sofs/urls.py
@@ -1,9 +1,12 @@
+from bank_sofs.views.list import BankSofsListView
+from bank_sofs.views.create import BankSofsCreateView
+
 from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
-from .views.list import BankSofsListView
 
 app_name = 'bank_sofs'
 
 urlpatterns = [
-    url(r'^bank/$', login_required(BankSofsListView.as_view(), login_url='login'), name="bank_list"),
+    url(r'^bank/$', login_required(BankSofsListView.as_view(), login_url='login'), name="bank_sofs_list"),
+    url(r'^bank/create$', login_required(BankSofsCreateView.as_view(), login_url='login'), name="bank_sofs_create"),
 ]
