ac79c9b0652848ddcf2d7b3a2f0f5ed6c6fd338a
wutcharin munjai <wutcharin.mun@ascendcorp.com>
2017-05-31 15:54:05 +0700

    [EQTR-1250] Can add bank profile -  reviewed by Nun

--- web_admin/bank_sofs/urls.py
+++ web_admin/bank_sofs/urls.py
@@ -1,5 +1,5 @@
 from bank_sofs.views.list import BankSofsListView
-from bank_sofs.views.create import BankSofsCreateView
+from bank_sofs.views.create import BankSOFsCreateView
 
 from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
@@ -8,5 +8,5 @@ app_name = 'bank_sofs'
 
 urlpatterns = [
     url(r'^bank/$', login_required(BankSofsListView.as_view(), login_url='login'), name="bank_sofs_list"),
-    url(r'^bank/create$', login_required(BankSofsCreateView.as_view(), login_url='login'), name="bank_sofs_create"),
+    url(r'^bank/create$', login_required(BankSOFsCreateView.as_view(), login_url='login'), name="bank_sofs_create"),
 ]
--- web_admin/bank_sofs/views/create.py
+++ web_admin/bank_sofs/views/create.py
@@ -1,28 +1,33 @@
 from web_admin.restful_methods import RESTfulMethods
+from web_admin.api_settings import GET_ALL_CURRENCY_URL
 
+from django.conf import settings
+from django.contrib import messages
 from django.views.generic.base import TemplateView
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect
 
 import logging
 
 logger = logging.getLogger(__name__)
 
 
-class BankSofsCreateView(TemplateView, RESTfulMethods):
-    template_name = "bank_sofs/create.html"
-    url = "api-gateway/sof-bank/v1/banks"
+class BankSOFsCreateView(TemplateView, RESTfulMethods):
+    template_name = "bank_sof/create.html"
+    url = settings.DOMAIN_NAMES + "api-gateway/sof-bank/v1/banks"
 
     def get_context_data(self, **kwargs):
         logger.info('========== Start create bank sofs ==========')
-        context = super(BankSofsCreateView, self).get_context_data(**kwargs)
+        currencies = self._get_currencies_list()
+        context = {'currencies': currencies}
         logger.info('========== Finished create bank sofs ==========')
         return context
 
     def post(self, request, *args, **kwargs):
         logger.info('========== Start creating bank profile ==========')
+
         name = request.POST.get('name')
         bank_bin = request.POST.get('bin')
-        is_active = request.POST.get('is_active')
+        is_active = bool(request.POST.get('is_active'))
         description = request.POST.get('description')
         credit_url = request.POST.get('credit_url')
         debit_url = request.POST.get('debit_url')
@@ -45,6 +50,24 @@ class BankSofsCreateView(TemplateView, RESTfulMethods):
         data, success = self._post_method(api_path=self.url,
                                           func_description="Bank Profile",
                                           logger=logger, params=params)
+        if success:
+            logger.info('========== Finished creating bank profile ==========')
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                'Add bank successfully'
+            )
+            return redirect('bank_sofs:bank_sofs_list')
 
-        logger.info('========== Finished creating bank profile ==========')
-        return render(request, 'bank_sofs/list.html')
+    def _get_currencies_list(self):
+        url = GET_ALL_CURRENCY_URL
+        data, success = self._get_method(api_path=url,
+                                         func_description="currency list from backend",
+                                         logger=logger,
+                                         is_getting_list=True)
+        if success:
+            value = data.get('value', '')
+            currencies = [i.split('|') for i in value.split(',')]
+        else:
+            currencies = []
+        return currencies
