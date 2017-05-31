610a8713bcc6eb5d05780d50607bcb95dd6c9864
trananhkma <trananhkma@gmail.com>
2017-05-31 16:32:35 +0700

    [EQTR-1447] fix bug side effect of adding search page - Agent List [Reviewed by Alex]

--- web_admin/agents/views/list.py
+++ web_admin/agents/views/list.py
@@ -1,8 +1,6 @@
 import logging
 
 from datetime import datetime
-from django.http import JsonResponse
-from django.template.loader import render_to_string
 from django.views.generic.base import TemplateView
 from web_admin.restful_methods import RESTfulMethods
 from django.shortcuts import render
@@ -18,7 +16,7 @@ class ListView(TemplateView, RESTfulMethods):
 
     template_name = 'agents/list.html'
 
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         logger.info('========== Start showing Agent List page ==========')
         context = {'agent_update_msg': self.request.session.pop('agent_update_msg', None)}
         logger.info('========== Finished showing Agent List page ==========')
@@ -27,37 +25,45 @@ class ListView(TemplateView, RESTfulMethods):
     def post(self, request, *args, **kwargs):
         logger.info('========== Start searching agent ==========')
         # Get params
-        unique_reference = request.POST.get('unique_reference', '')
-        email = request.POST.get('email', '')
-        primary_mobile_number = request.POST.get('primary_mobile_number', '')
-        kyc_status = request.POST.get('kyc_status', '')
-        from_created_timestamp = request.POST.get('from_created_timestamp', '')
-        to_created_timestamp = request.POST.get('to_created_timestamp', '')
+        unique_reference = request.POST.get('unique_reference')
+        email = request.POST.get('email')
+        primary_mobile_number = request.POST.get('primary_mobile_number')
+        kyc_status = request.POST.get('kyc_status')
+        from_created_timestamp = request.POST.get('from_created_timestamp')
+        to_created_timestamp = request.POST.get('to_created_timestamp')
 
         # Build body
+        context = {}
         body = {}
-        if unique_reference is not '':
+        if unique_reference:
             body['unique_reference'] = unique_reference
-        if email is not '':
+        if email:
             body['email'] = email
-        if primary_mobile_number is not '':
+        if primary_mobile_number:
             body['primary_mobile_number'] = primary_mobile_number
-        if kyc_status is not '':
-            if kyc_status.lower() == "true":
-                kyc_status = True
-            else:
-                kyc_status = False
-            body['kyc_status'] = kyc_status
 
-        if from_created_timestamp is not '':
-            from_created_timestamp = datetime.strptime(from_created_timestamp, "%Y-%m-%d")
-            from_created_timestamp = from_created_timestamp.strftime('%Y-%m-%dT%H:%M:%SZ')
-            body['from_created_timestamp'] = from_created_timestamp
+        context.update(body)
+
+        if kyc_status and isinstance(kyc_status, str):
+            if kyc_status.lower() == "true":
+                new_kyc_status = True
+            else:
+                new_kyc_status = False
+            body['kyc_status'] = new_kyc_status
+            context['kyc_status'] = kyc_status
+
+        if from_created_timestamp:
+            new_from_created_timestamp = datetime.strptime(from_created_timestamp, "%Y-%m-%d")
+            new_from_created_timestamp = new_from_created_timestamp.strftime('%Y-%m-%dT%H:%M:%SZ')
+            body['from_created_timestamp'] = new_from_created_timestamp
+            context['from_created_timestamp'] = from_created_timestamp
+
         if to_created_timestamp is not '':
-            to_created_timestamp = datetime.strptime(to_created_timestamp, "%Y-%m-%d")
-            to_created_timestamp = to_created_timestamp.replace(hour=23, minute=59, second=59)
-            to_created_timestamp = to_created_timestamp.strftime('%Y-%m-%dT%H:%M:%SZ')
-            body['to_created_timestamp'] = to_created_timestamp
+            new_to_created_timestamp = datetime.strptime(to_created_timestamp, "%Y-%m-%d")
+            new_to_created_timestamp = new_to_created_timestamp.replace(hour=23, minute=59, second=59)
+            new_to_created_timestamp = new_to_created_timestamp.strftime('%Y-%m-%dT%H:%M:%SZ')
+            body['to_created_timestamp'] = new_to_created_timestamp
+            context['to_created_timestamp'] = to_created_timestamp
 
         api_path = SEARCH_AGENT
 
@@ -67,7 +73,6 @@ class ListView(TemplateView, RESTfulMethods):
             logger=logger,
             params=body
         )
-        context = {'data': data}
-        list_content = render_to_string("agents/list_content.html", context)
+        context['data'] = data
         logger.info('========== Finished searching agent ==========')
-        return JsonResponse({"status": status, "table_content": list_content})
+        return render(request, self.template_name, context)
