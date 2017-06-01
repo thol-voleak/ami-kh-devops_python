2d7c926d46e19003b63548fb3dfb60efa7b1d9b0
Pham Manh Hung <hung.pham@ascendcorp.com>
2017-06-01 18:18:27 +0700

    [EQTR-1474] - [Web] System user can update SPI URL by service command - API updated

--- web_admin/services/views/spi/update.py
+++ web_admin/services/views/spi/update.py
@@ -24,6 +24,8 @@ class SPIUpdate(TemplateView, RESTfulMethods):
 
         spi_type = request.POST.get('spi_url_type')
         spi_url = request.POST.get('spi_url_value')
+        connection_timeout = request.POST.get('connection_timeout')
+        read_timeout = request.POST.get('read_timeout')
 
         params = {
            "service_command_id":service_command_id,
@@ -34,6 +36,16 @@ class SPIUpdate(TemplateView, RESTfulMethods):
            "max_retry":8,
            "retry_delay_millisecond":785
         }
+
+        if connection_timeout.isdigit() and connection_timeout != '0':
+            params['connection_timeout'] = connection_timeout
+        else:
+            params['connection_timeout'] = 3600
+
+        if read_timeout.isdigit() and read_timeout != '0':
+            params['read_timeout'] = read_timeout
+        else:
+            params['read_timeout'] = 3600
 
         data, success = self.update_spi(spiUrlId, params)
         logger.info("========== Finish updating SPI URL by service command ==========")
