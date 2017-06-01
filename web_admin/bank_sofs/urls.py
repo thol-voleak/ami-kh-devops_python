c2a915b803f3d8ae036501b0ebe7eb793121d5a7
boss <abhichet.ketsiri@gmail.com>
2017-06-01 09:48:51 +0700

    [EQTR-1355] Can view bank sof detail on web admin portal - [Reviewed by Klang]

--- web_admin/bank_sofs/urls.py
+++ web_admin/bank_sofs/urls.py
@@ -2,6 +2,7 @@ from bank_sofs.views.list import BankSofsListView
 from bank_sofs.views.create import BankSOFsCreateView
 from bank_sofs.views.edit import BankSOFsUpdateView
 
+from bank_sofs.views.detail import BankSOFsDetailView
 from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
 
@@ -11,4 +12,5 @@ urlpatterns = [
     url(r'^bank/$', login_required(BankSofsListView.as_view(), login_url='login'), name="bank_sofs_list"),
     url(r'^bank/create$', login_required(BankSOFsCreateView.as_view(), login_url='login'), name="bank_sofs_create"),
     url(r'^bank/(?P<bank_id>[0-9A-Za-z]+)/edit$', login_required(BankSOFsUpdateView.as_view(), login_url='login'), name="bank_sofs_edit"),
+    url(r'^bank/(?P<bank_id>[0-9A-Za-z]+)/detail$', login_required(BankSOFsDetailView.as_view(), login_url='login'), name="bank_sofs_detail"),
 ]
