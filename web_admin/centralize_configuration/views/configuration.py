78a104f1fca9ee5ae99bbee92af8f3d3e877f577
nam_pham_vn <nam.pham@ascendcorp.com>
2017-06-08 09:32:02 +0700

    [EQTR-1705] [Web][Success]: fix token error [reviewed by Gate]

--- web_admin/centralize_configuration/views/configuration.py
+++ web_admin/centralize_configuration/views/configuration.py
@@ -14,69 +14,59 @@ class ConfigurationListView(TemplateView, RESTfulMethods):
     template_name = 'centralize_configuration/configuration_list.html'
 
     def get_context_data(self, **kwargs):
-        try:
-            logger.info('========== Start getting all configuration scope ==========')
-            context = super(ConfigurationListView, self).get_context_data(**kwargs)
-            scope = context['scope']
-            url = api_settings.CONFIGURATION_URL.format(scope=scope)
-            data, success = self._get_method(url, 'configuration scope', logger)
-            if success:
-                logger.info('========== Finish getting all configuration scope ==========')
-                context["configurations"] = data
-                context["scope_name"] = scope
-                return context
-        except Exception as e:
-            raise Exception(e)
+        logger.info('========== Start getting all configuration scope ==========')
+        context = super(ConfigurationListView, self).get_context_data(**kwargs)
+        scope = context['scope']
+        url = api_settings.CONFIGURATION_URL.format(scope=scope)
+        data, success = self._get_method(url, 'configuration scope', logger)
+        if success:
+            logger.info('========== Finish getting all configuration scope ==========')
+            context["configurations"] = data
+            context["scope_name"] = scope
+            return context
 
 
 class ConfigurationDetailsView(TemplateView, RESTfulMethods):
     template_name = 'centralize_configuration/configuration_details.html'
 
     def get_context_data(self, **kwargs):
-        try:
-            logger.info('========== Start getting configuration scope details ==========')
-            context = super(ConfigurationDetailsView, self).get_context_data(**kwargs)
-            scope = context['scope']
-            conf_key = context['conf_key']
-            url = api_settings.CONFIGURATION_DETAIL_URL.format(scope=scope, key=conf_key)
-
-            data, success = self._get_method(url, 'configuration scope details', logger)
-            if success:
-                logger.info(
-                    '========== Finish getting configuration scope details ==========')
-                context["configurations"] = data
-                context["scope_name"] = scope
-                return context
-        except Exception as e:
-            return Exception(e)
+        logger.info('========== Start getting configuration scope details ==========')
+        context = super(ConfigurationDetailsView, self).get_context_data(**kwargs)
+        scope = context['scope']
+        conf_key = context['conf_key']
+        url = api_settings.CONFIGURATION_DETAIL_URL.format(scope=scope, key=conf_key)
+        
+        
+        data, success = self._get_method(url, 'configuration scope details', logger)
+        if success:
+            logger.info(
+                '========== Finish getting configuration scope details ==========')
+            context["configurations"] = data
+            context["scope_name"] = scope
+            return context
 
     def post(self, request, *args, **kwargs):
-        try:
-            logger.info('========== Start updating configuration scope ==========')
-            scope = kwargs.get('scope', None)
-            conf_key = kwargs.get('conf_key', None)
-            conf_value = request.POST.get('conf_value')
-
-            url = settings.DOMAIN_NAMES + api_settings.CONFIGURATION_DETAIL_URL.format(
-                scope=scope, key=conf_key)
-            params = {
-                'value': conf_value
-            }
-
-            data, success = self._put_method(url, 'configuration scope', logger, params)
-            if success:
-                messages.add_message(
-                    request,
-                    messages.ERROR,
-                    'Please restart service to get configuration effect.'
-                )
-            else:
-                messages.add_message(
-                    request,
-                    messages.ERROR,
-                    'Something wrong happened.'
-                )
-            logger.info('========== Finish updating configuration scope ==========')
-            return redirect('centralize_configuration:configuration_list', scope=scope)
-        except Exception as e:
-            raise Exception(e)
+        logger.info('========== Start updating configuration scope ==========')
+        scope = kwargs.get('scope', None)
+        conf_key = kwargs.get('conf_key', None)
+        conf_value = request.POST.get('conf_value')
+        
+        url = settings.DOMAIN_NAMES + api_settings.CONFIGURATION_DETAIL_URL.format(
+                       scope=scope, key=conf_key)
+        params = {'value': conf_value}
+        
+        data, success = self._put_method(url, 'configuration scope', logger, params)
+        if success:
+            messages.add_message(
+                request,
+                messages.ERROR,
+                'Please restart service to get configuration effect.'
+                  )
+        else:
+            messages.add_message(
+                request,
+                messages.ERROR,
+                'Something wrong happened.'
+                   )
+        logger.info('========== Finish updating configuration scope ==========')
+        return redirect('centralize_configuration:configuration_list', scope=scope)
