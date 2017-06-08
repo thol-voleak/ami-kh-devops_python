765621768ee2191c2e939255055538a2d9798a89
boss <abhichet.ketsiri@gmail.com>
2017-06-08 11:40:57 +0700

    [EQTR-1269] system user can search the fund in transaction - Reviewed by P'Klang

--- web_admin/bank_sofs/urls.py
+++ web_admin/bank_sofs/urls.py
@@ -3,6 +3,7 @@ from bank_sofs.views.bank.delete import DeleteView
 from bank_sofs.views.bank.edit import EditView
 from bank_sofs.views.bank.list import ListView
 from bank_sofs.views.sof.bank_sof_list import BankSOFView
+from bank_sofs.views.sof.bank_sof_transaction import BankSOFTransaction
 
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
@@ -18,4 +19,5 @@ urlpatterns = [
     url(r'^sofs/(?P<bank_id>[0-9A-Za-z]+)/edit/$', login_required(EditView.as_view(), login_url='login'), name="bank_sofs_edit"),
     url(r'^sofs/(?P<bank_id>[0-9A-Za-z]+)/detail/$', login_required(DetailsView.as_view(), login_url='login'), name="bank_sofs_detail"),
     url(r'^sofs/(?P<bank_id>[0-9A-Za-z]+)/delete/$', login_required(DeleteView.as_view(), login_url='login'), name="bank_sofs_delete"),
+    url(r'^sofs/transaction/$', login_required(BankSOFTransaction.as_view(), login_url='login'), name="bank_sofs_transaction"),
 ]
