ca8417f897b4be245a7eff2256627ce7609bac4a
Phan Tuong <phantuong@ip-192-168-0-35.ap-southeast-1.compute.internal>
2017-06-28 10:34:11 +0700

    [EQTR-2322] [WEB] System user can search all sof reconcile file list - [Reviewed by Klang]

--- web_admin/reconcile/urls.py
+++ web_admin/reconcile/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
 
 from .views.partner_file_list import PartnerFileList
+from .views.sof_file_list import SofFileList
 
 app_name = 'reconcile'
 
@@ -10,7 +11,7 @@ urlpatterns = [
                                                 login_url='authentications:login'), name="reconcile_partner_file_list"),
     url(r'^partner/report/$', login_required(PartnerFileList.as_view(),
                                                 login_url='authentications:login'), name="reconcile_partner_report"),
-    url(r'^sof/file-list/$', login_required(PartnerFileList.as_view(),
+    url(r'^sof/file-list/$', login_required(SofFileList.as_view(),
                                                 login_url='authentications:login'), name="reconcile_sof_file_list"),
     url(r'^sof/report/$', login_required(PartnerFileList.as_view(),
                                                 login_url='authentications:login'), name="reconcile_partner_report"),
--- web_admin/web_admin/api_settings.py
+++ web_admin/web_admin/api_settings.py
@@ -130,3 +130,4 @@ CASH_SOFS_URL = "api-gateway/report/v1/cash/sofs"
 
 # Reconcile
 SEARCH_RECONCILE_PARTNER_FILE_LIST = 'api-gateway/report/v1/reconciled/partners'
+SEARCH_RECONCILE_SOF_FILE_LIST = 'api-gateway/report/v1/reconciled/sofs'
