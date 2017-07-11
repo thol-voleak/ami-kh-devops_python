c3a47a3653279b881c40be88cf49eea3cb6f7c3f
wutcharin munjai <wutcharin.mun@ascendcorp.com>
2017-07-11 09:46:31 +0700

    [EQTR-2296] - Delete Permission Entities

--- web_admin/authentications/urls.py
+++ web_admin/authentications/urls.py
@@ -2,6 +2,7 @@ from authentications.views.permissions.list import PermissionList
 from authentications.views.permissions.create import PermissionCreate
 from authentications.views.permissions.edit import PermissionEditView
 from authentications.views.permissions.detail import PermissionDetailView
+from authentications.views.permissions.delete import PermissionDeleteView
 from authentications.views.auth import login_user, logout_user
 
 from django.conf.urls import url
@@ -14,6 +15,7 @@ urlpatterns = [
     url(r'^permissions/list$', PermissionList.as_view(), name='permissions_list'),
     url(r'^permissions/create$', PermissionCreate.as_view(), name='create_permission'),
     url(r'^permissions/(?P<permission_id>[0-9A-Za-z]+)/edit$', PermissionEditView.as_view(), name='edit_permission'),
+    url(r'^permissions/(?P<permission_id>[0-9A-Za-z]+)/delete', PermissionDeleteView.as_view(), name='delete_permission'),
     url(r'^permissions/(?P<permission_id>[0-9A-Za-z]+)/details', PermissionDetailView.as_view(),
         name='permission_detail'),
 ]
