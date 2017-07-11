8b26ef1606ea0ae0b9efd1521f28505102ea304e
wutcharin munjai <wutcharin.mun@ascendcorp.com>
2017-07-11 11:05:03 +0700

    [EQTR-2300] - Create new Role

---
+++ web_admin/authentications/views/roles/create.py
@@ -0,0 +1,55 @@
+from authentications.utils import get_auth_header
+from web_admin import api_settings
+from web_admin import setup_logger, RestFulClient
+
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.views.generic.base import TemplateView
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class RoleCreate(TemplateView):
+    template_name = "roles/create.html"
+    logger = logger
+
+    def dispatch(self, request, *args, **kwargs):
+        self.logger = setup_logger(self.request, logger)
+        return super(RoleCreate, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(RoleCreate, self).get_context_data(**kwargs)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.logger.info('========== Start creating role ==========')
+
+        name = request.POST.get('name', '')
+        description = request.POST.get('description', '')
+
+        params = {
+            'name': name,
+            'description': description,
+            'is_page_level': True
+        }
+
+        is_success, status_code, status_message, data = RestFulClient.post(self.request,
+                                                                           api_settings.CREATE_ROLE_PATH,
+                                                                           self._get_headers(), logger, params)
+        if is_success:
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                'Created role entity successfully'
+            )
+            self.logger.info('========== End creating role ==========')
+            return redirect('authentications:role_list')
+        else:
+            return render(request, self.template_name)
+
+    def _get_headers(self):
+        if getattr(self, '_headers', None) is None:
+            self._headers = get_auth_header(self.request.user)
+        return self._headers
