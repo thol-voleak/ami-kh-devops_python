4c43ad9253c208143ec3ac0863d45785616d5d35
Alex - lien.ngo <lien.ngo@ascendcorp.com>
2017-07-20 17:03:39 +0700

    [EQTR-2243] Fix bug “setup_logger” - [reviewed by Lucian]

--- web_admin/customers/views/reset_identity_password.py
+++ web_admin/customers/views/reset_identity_password.py
@@ -3,14 +3,15 @@ from web_admin import api_settings
 from web_admin import ajax_functions
 import logging
 from web_admin.utils import setup_logger
-
+from authentications.utils import get_correlation_id_from_username
 
 # logger = logging.getLogger(__name__)
 
 
 def reset_password(request, customer_id, identity_id):
     logger = logging.getLogger(__name__)
-    logger = setup_logger(request, logger)
+    correlation_id = get_correlation_id_from_username(request.user)
+    logger = setup_logger(request, logger, correlation_id)
     logger.info('========== Start reset password==========')
     url = settings.DOMAIN_NAMES + api_settings.RESET_IDENTITY_PASSWORD.format(customer_id, identity_id)
     params = {}
