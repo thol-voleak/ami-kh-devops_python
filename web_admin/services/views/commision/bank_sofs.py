9edeaa800134226d9b6fa2c9a4830df9409a5027
Alex - lien.ngo <lien.ngo@ascendcorp.com>
2017-07-24 10:45:06 +0700

    [EQTR-2734] [Web] cannot load Specific SOF for Agent Hierrachy Distribution Fee - [reviewed by Lucian]

--- web_admin/services/views/commision/bank_sofs.py
+++ web_admin/services/views/commision/bank_sofs.py
@@ -1,12 +1,13 @@
 import logging
-from django.conf import settings
 from web_admin.api_settings import BANK_SOFS_URL
 from web_admin import ajax_functions
 from authentications.utils import get_correlation_id_from_username
+from web_admin import setup_logger
 
 def bank_sofs(request, user_id):
     logger = logging.getLogger(__name__)
-    logger = setup_logger(request, logger)
+    correlation_id = get_correlation_id_from_username(request.user)
+    logger = setup_logger(request, logger, correlation_id)
     url = BANK_SOFS_URL
     params = {'user_id': user_id, 'user_type_id': 2};
     result = ajax_functions._post_method(request, url, "", logger, params)
