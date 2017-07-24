8c20b8d2efc38adcfd6b3fccf6beae1cb9665913
tung <quangtung.nguyen@ascendcorp.com>
2017-07-24 09:45:07 +0700

    [EQTR-2645] [UI][Success case] System user can get card type detail (Review by Tee)

--- web_admin/card_type/urls.py
+++ web_admin/card_type/urls.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
+
+from card_type.views.detail import CardTypeDetail
 from .views.list import ListView
 
 app_name = 'card_type'
 
 urlpatterns = [
     url(r'^list$', login_required(ListView.as_view(), login_url='authentications:login'), name="card-type-list"),
+    url(r'^detail/(?P<card_type_id>[0-9A-Za-z]+)/$', login_required(CardTypeDetail.as_view(),
+                                                                               login_url='authentications:login'), name="card_type_detail")
 ]
