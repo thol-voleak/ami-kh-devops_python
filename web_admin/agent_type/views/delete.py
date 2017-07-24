b53bcfbdf30c5773dd4d22ab1ae814b258b93791
nam_pham_vn <nam.pham@ascendcorp.com>
2017-07-24 13:08:37 +0700

    [EQTR-2723] [Web][Fail] error at delete agent type page [reviewed by Gate]

--- web_admin/agent_type/views/delete.py
+++ web_admin/agent_type/views/delete.py
@@ -35,8 +35,8 @@ class DeleteView(GroupRequiredMixin, TemplateView, RESTfulMethods):
         self.logger.info('========== Start showing Delete Agent Type page ==========')
         try:
             context = super(DeleteView, self).get_context_data(**kwargs)
-            agent_type_id = context['agent_type_id']
-            self.logger.info('========== Finished showing Delete Agent Type page ==========')
+            agent_type_id = context['agent_type_id']  
+            self.logger.info('========== Finished showing Delete Agent Type page ==========')          
             return self._get_agent_type_detail(agent_type_id)
         except Exception as e:
             self.logger.error(e)
@@ -49,7 +49,7 @@ class DeleteView(GroupRequiredMixin, TemplateView, RESTfulMethods):
         agent_type_id = kwargs['agent_type_id']
 
         data, success = self._delete_method(api_path=DELETE_AGENT_TYPE_URL.format(agent_type_id),
-                                            func_description="Balance Distribution",
+                                            func_description="Delete agent type",
                                             logger=logger)
         if success:
             request.session['agent_type_delete_msg'] = 'Deleted data successfully'
@@ -61,7 +61,9 @@ class DeleteView(GroupRequiredMixin, TemplateView, RESTfulMethods):
         return success
 
     def _get_agent_type_detail(self, agent_type_id):
-        data, success = self._get_method(api_path=AGENT_TYPE_DETAIL_URL.format(agent_type_id),
+        param = {'id': agent_type_id}
+        data, success = self._post_method(api_path=AGENT_TYPE_DETAIL_URL,
                                          func_description="Agent Type",
-                                         logger=logger)
-        return {'agent_type_info': data}
+                                         logger=logger,
+                                         params = param)
+        return {'agent_type_info': data[0]}
