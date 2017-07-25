7e45b764231b20b772fabd873f6fdf96185c4b29
wutcharin munjai <wutcharin.mun@ascendcorp.com>
2017-07-25 16:05:56 +0700

    [EQTR-2393] Update to check permission for country code (Reviewed by Nun)

--- web_admin/balances/views/country_code.py
+++ web_admin/balances/views/country_code.py
@@ -6,7 +6,7 @@ from web_admin.restful_methods import RESTfulMethods
 from web_admin import ajax_functions, setup_logger
 from web_admin.api_settings import ADD_COUNTRY_CODE_URL, GLOBAL_CONFIGURATIONS_URL
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 import copy
 import logging
@@ -14,18 +14,9 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class CountryCode(GroupRequiredMixin, TemplateView, RESTfulMethods):
-    group_required = "SYS_MANAGE_COUNTRYCODE"
-    login_url = 'web:permission_denied'
-    raise_exception = False
-
+class CountryCode(TemplateView, RESTfulMethods):
     template_name = "country/country_code.html"
     logger = logger
-
-    def check_membership(self, permission):
-        self.logger.info(
-            "Checking permission for [{}] username with [{}] permission".format(self.request.user, permission))
-        return check_permissions_by_user(self.request.user, permission[0])
 
     def dispatch(self, request, *args, **kwargs):
         correlation_id = get_correlation_id_from_username(self.request.user)
@@ -33,6 +24,9 @@ class CountryCode(GroupRequiredMixin, TemplateView, RESTfulMethods):
         return super(CountryCode, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
+        if not check_permissions_by_user(self.request.user, "SYS_VIEW_COUNTRYCODE"):
+            return redirect('web:permission_denied')
+
         url = GLOBAL_CONFIGURATIONS_URL
         data, success = self._get_method(api_path=url,
                                          func_description="global configurations",
@@ -46,6 +40,9 @@ class CountryCode(GroupRequiredMixin, TemplateView, RESTfulMethods):
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
+        if not check_permissions_by_user(self.request.user, "SYS_CREATE_COUNTRYCODE"):
+            return redirect('web:permission_denied')
+
         self.logger.info('========== Start add country code ==========')
         country_code = request.POST.get('country_code')
         params = {
