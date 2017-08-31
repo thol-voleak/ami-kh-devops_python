498c148b23273645763f641c293f35f6ed1aab29
Thang <duythangdoan1994@gmail.com>
2017-08-31 10:00:59 +0700

    EQTR-3747 Add version on the right top conner of admin portal web Review by Frank

--- web_admin/services/views/services_list.py
+++ web_admin/services/views/services_list.py
@@ -1,7 +1,7 @@
 from authentications.utils import get_correlation_id_from_username, check_permissions_by_user
 from braces.views import GroupRequiredMixin
 from web_admin import setup_logger
-from web_admin.api_settings import SERVICE_LIST_URL
+from web_admin.api_settings import SERVICE_LIST_URL, SERVICE_GROUP_LIST_URL
 from web_admin.restful_methods import RESTfulMethods
 from django.views.generic.base import TemplateView
 
@@ -29,7 +29,12 @@ class ListView(GroupRequiredMixin, TemplateView, RESTfulMethods):
         return super(ListView, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
-        data = self.get_services_list()
+        service_list = self.get_services_list()
+        service_group_list = self.get_service_group_list()
+        for i in service_list:
+            for j in service_group_list:
+                if i['service_group_id'] == j['service_group_id']:
+                    i['service_group_name'] = j['service_group_name']
 
         permissions = {}
         permissions['CAN_VIEW_SERVICE'] = check_permissions_by_user(self.request.user, 'CAN_VIEW_SERVICE')
@@ -38,11 +43,26 @@ class ListView(GroupRequiredMixin, TemplateView, RESTfulMethods):
         permissions['CAN_DELETE_SERVICE'] = check_permissions_by_user(self.request.user, 'CAN_DELETE_SERVICE')
         permissions['CAN_ADD_SERVICE'] = check_permissions_by_user(self.request.user, 'CAN_ADD_SERVICE')
 
-        result = {'data': data,
+        result = {'data': service_list,
                   'permissions': permissions}
         return result
 
     def get_services_list(self):
         url = SERVICE_LIST_URL
         data, success = self._get_method(api_path=url, func_description="service list", is_getting_list=True)
+        return data
+
+    def get_service_group_list(self):
+        url = SERVICE_GROUP_LIST_URL
+        data, success = self._get_method(url, "service group list", logger, True)
+        is_permission_detail = check_permissions_by_user(self.request.user, 'CAN_VIEW_SERVICE_GROUP')
+        is_permission_edit = check_permissions_by_user(self.request.user, 'CAN_EDIT_SERVICE_GROUP')
+        is_permission_delete = check_permissions_by_user(self.request.user, 'CAN_DELETE_SERVICE_GROUP')
+
+        if success:
+            self.logger.info('========== Finished get get bank list ==========')
+            for i in data:
+                i['is_permission_detail'] = is_permission_detail
+                i['is_permission_edit'] = is_permission_edit
+                i['is_permission_delete'] = is_permission_delete
         return data
