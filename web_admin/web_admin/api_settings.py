91c0c0e254dd4541eed1010d9036a8815124462e
Alex - lien.ngo <lien.ngo@ascendcorp.com>
2017-09-14 10:57:34 +0700

    [EQTR-3930] update card type list - [reviewed by Lucian]

--- web_admin/web_admin/api_settings.py
+++ web_admin/web_admin/api_settings.py
@@ -177,6 +177,8 @@ UPDATE_CARD_TYPE = 'api-gateway/prepaid-card/'+API_VERSION+'/admin/card-types/{c
 SEARCH_CARD_PROVIDER ='api-gateway/report/'+API_VERSION+'/cards/sofs/providers'
 CREATE_CARD_DESIGN = 'api-gateway/sof-card/'+API_VERSION+'/admin/providers/{provider_id}/card-designs'
 CARD_DESIGN_DETAIL = 'api-gateway/sof-card/'+API_VERSION+'/admin/providers/{provider_id}/card-designs/{card_id}'
+CARD_TYPE_LIST = 'api-gateway/sof-card/'+API_VERSION+'/admin/card-types'
+
 
 # bank profile
 CREATE_BANK_PROFILE_PATH = "api-gateway/sof-bank/" + API_VERSION + "/admin/banks"
