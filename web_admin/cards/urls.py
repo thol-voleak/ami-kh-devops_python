d15c942b866b09e5cc69bdf47b17765c912c3b8d
abhichet ketsiri <apichet.ket@ascendcorp.com>
2017-09-19 15:06:34 +0700

    [EQTR-4002][Rule 2: UI] Create additional action buttons to let Fraud Officers Unfreeze Virtual Card - Create a page for list freeze card and can unfreeze card. Review by P'Klang

--- web_admin/cards/urls.py
+++ web_admin/cards/urls.py
@@ -2,10 +2,16 @@ from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
 from .views.profile import ProfileView
 from .views.history import HistoryView
+from .views.card_freeze_list import CardFreezeList
 
 app_name = 'cards'
 
 urlpatterns = [
     url(r'^profile/$', login_required(ProfileView.as_view(), login_url='authentications:login'), name="profile"),
     url(r'^history/$', login_required(HistoryView.as_view(), login_url='authentications:login'), name="history"),
+    url(r'^card-freeze-list/$', login_required(CardFreezeList.as_view(), login_url='authentications:login'),
+        name="card_freeze_list"),
+    url(r'^card-freeze-list/delete/(?P<id>[^/]+)/$', login_required(CardFreezeList.as_view(), login_url='authentications:login'),
+        name="delete_card_freeze"),
+
 ]
