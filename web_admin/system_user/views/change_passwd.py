67cd69b468d53cb193b57fa98605e78cdb566531
Alex - lien.ngo <lien.ngo@ascendcorp.com>
2017-09-28 15:28:03 +0700

    [EQTR-4249]  clean code - [reviewed by Lucian]

--- web_admin/system_user/views/change_passwd.py
+++ web_admin/system_user/views/change_passwd.py
@@ -45,10 +45,18 @@ class ChangePasswd(TemplateView, GetHeaderMixin):
             params=body)
         if success:
             update_passwd_success = True
+            self.logger.info('========== User finish updating password ==========')
+            return render(request, self.template_name, context={'update_passwd_success': update_passwd_success})
         else:
             if status_code in ["access_token_expire", 'authentication_fail', 'invalid_access_token']:
                 self.logger.info("{}".format(message))
                 raise InvalidAccessToken(message)
+
+            body = {
+                'old_password': old_password,
+                'new_password': new_password,
+                "confirm_new_password": request.POST.get('confirm_new_password')
+            }
 
             if status_code.lower() in ["invalid_request"]:
                 messages.add_message(
@@ -56,31 +64,17 @@ class ChangePasswd(TemplateView, GetHeaderMixin):
                     messages.ERROR,
                     "Password change failed as it not met password criteria. Please review and try again."
                 )
-                body = {
-                    'old_password': old_password,
-                    'new_password': new_password,
-                    "confirm_new_password": request.POST.get('confirm_new_password')
-                }
 
                 self.logger.info('========== User finish updating password ==========')
                 return render(request, self.template_name, body)
 
-            if status_code.lower() in ["general_error"]:
-                messages.add_message(
-                    request,
-                    messages.ERROR,
-                    "Password change failed. Please try again or contact support."
-                )
-                body = {
-                    'old_password': old_password,
-                    'new_password': new_password,
-                    "confirm_new_password": request.POST.get('confirm_new_password')
-                }
+            messages.add_message(
+                request,
+                messages.ERROR,
+                "Password change failed. Please try again or contact support."
+            )
 
-                self.logger.info('========== User finish updating password ==========')
-                return render(request, self.template_name, body)
+            self.logger.info('========== User finish updating password ==========')
+            return render(request, self.template_name, body)
 
 
-
-        self.logger.info('========== User finish updating password ==========')
-        return render(request, self.template_name, context={'update_passwd_success': update_passwd_success})
