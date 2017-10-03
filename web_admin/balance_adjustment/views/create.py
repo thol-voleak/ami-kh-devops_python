2025875e05c3e5c688a46f703741982388d2629e
Alex - lien.ngo <lien.ngo@ascendcorp.com>
2017-10-03 15:12:15 +0700

    [EQTR-4390, EQTR-4391] [Web] Error message handling on 'Balance Adjustment Execution' page - [reviewed by Lucian]

--- web_admin/balance_adjustment/views/create.py
+++ web_admin/balance_adjustment/views/create.py
@@ -1,19 +1,16 @@
 from authentications.utils import get_correlation_id_from_username, check_permissions_by_user
-
 import ast
 from django.views.generic.base import TemplateView
 from django.shortcuts import render, redirect
 from braces.views import GroupRequiredMixin
-
 from django.contrib import messages
 from authentications.apps import InvalidAccessToken
 from web_admin import api_settings, setup_logger, RestFulClient
 from web_admin.get_header_mixins import GetHeaderMixin
-
+from django.contrib import messages
 import logging
 
 logger = logging.getLogger(__name__)
-
 
 class BalanceAdjustmentCreateView(GroupRequiredMixin, TemplateView, GetHeaderMixin):
     group_required = "SYS_BAL_ADJUST_REQUEST"
@@ -113,22 +110,7 @@ class BalanceAdjustmentCreateView(GroupRequiredMixin, TemplateView, GetHeaderMix
         }
 
         self.logger.info('Params: {}'.format(params))
-        context.update({
-            'agent_id': agent_id,
-            'initiator_source_of_fund_id': initiator_source_of_fund_id,
-            'product_reference_1': product_reference_1,
-            'product_reference_2': product_reference_2,
-            'product_reference_3': product_reference_3,
-            'payer_id': payer_id,
-            'payer_type': payer_type,
-            'payer_source_of_fund_id': payer_source_of_fund_id,
-            'payee_id': payee_id,
-            'payee_type': payee_type,
-            'payee_source_of_fund_id': payee_source_of_fund_id,
-            'amount': amount,
-            'service_name': service_name,
-            'product_name': product_name,
-        })
+
         success, status_code, message, data = RestFulClient.post(
             url=self.path,
             headers=self._get_headers(),
@@ -136,15 +118,48 @@ class BalanceAdjustmentCreateView(GroupRequiredMixin, TemplateView, GetHeaderMix
             params=params)
 
         self.logger.info('========== Finish create order balance adjustment ==========')
-        if not success:
+        if success:
+            messages.success(request, 'The adjustment is created successfully')
+            return redirect('balance_adjustment:balance_adjustment_list')
+        else:
             if status_code in ["access_token_expire", 'authentication_fail', 'invalid_access_token']:
                 self.logger.info("{}".format(message))
                 raise InvalidAccessToken(message)
-            else:
-                return (request, self.template_name, context)
 
-        messages.success(request, 'The adjustment is created successfully')
-        return redirect('balance_adjustment:balance_adjustment_list')
+            context = {
+                'agent_id': agent_id,
+                'initiator_source_of_fund_id': initiator_source_of_fund_id,
+                'product_reference_1': product_reference_1,
+                'product_reference_2': product_reference_2,
+                'product_reference_3': product_reference_3,
+                'payer_id': payer_id,
+                'payer_type': payer_type,
+                'payer_source_of_fund_id': payer_source_of_fund_id,
+                'payee_id': payee_id,
+                'payee_type': payee_type,
+                'payee_source_of_fund_id': payee_source_of_fund_id,
+                'amount': request.POST.get('amount'),
+                'service_name': service_name,
+                'product_name': product_name,
+            }
+
+            services = self.get_services_list()
+            context.update({'services': services})
+
+            if status_code.lower() in ["payment_not_allow"]:
+                messages.add_message(
+                    request,
+                    messages.ERROR,
+                    message
+                )
+            else:
+                messages.add_message (
+                    request,
+                    messages.ERROR,
+                    "Other error, please contact system administrator"
+                )
+
+            return render(request, self.template_name, context)
 
     def get_services_list(self):
         url = api_settings.SERVICE_LIST_URL
