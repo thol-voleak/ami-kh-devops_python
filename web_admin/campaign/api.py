b22b7aa3bfd6b6586272f23d7eb8bb6513f18df5
Bui Chi Hoa <chihoa.bui@ascendcorp.com>
2017-11-10 17:19:13 +0700

    [EQTR-5190] [UI] Delete mechanic permission control - reviewed by Leo

--- web_admin/campaign/api.py
+++ web_admin/campaign/api.py
@@ -1,11 +1,13 @@
 import logging
-from authentications.utils import get_correlation_id_from_username
+from authentications.utils import get_correlation_id_from_username, check_permissions_by_user
 from web_admin import api_settings, setup_logger
 from web_admin import ajax_functions
 
 
 class CampaignApi():
     def delete_mechanic_by_id(request, campaign_id, mechanic_id):
+        if not check_permissions_by_user(request.user, 'CAN_DELETE_MECHANIC'):
+            return
         logger = logging.getLogger(__name__)
         correlation_id = get_correlation_id_from_username(request.user)
         logger = setup_logger(request, logger, correlation_id)
