90898e8bbfd431cbf39c11fbbe3857db29176d25
Pham Manh Hung <hung.pham@ascendcorp.com>
2017-11-13 17:17:12 +0700

    [EQTR-5164] [UI] Add mechanic without required field - reviewed by Leo

--- web_admin/campaign/views/add_mechanic.py
+++ web_admin/campaign/views/add_mechanic.py
@@ -64,30 +64,56 @@ class AddMechanic(GroupRequiredMixin, TemplateView, GetHeaderMixin):
         self.logger.info('========== Start adding Mechanic ==========')
         context = super(AddMechanic, self).get_context_data(**kwargs)
         campaign_id = context['campaign_id']
-        str_start_date = request.POST.get('dtp_start_date')
-        str_end_date = request.POST.get('dtp_end_date')
-        start_time = request.POST.get('dtp_start_time')
-        end_time = request.POST.get('dtp_end_time')
+        input_start_date = request.POST.get('dtp_start_date')
+        input_end_date = request.POST.get('dtp_end_date')
+        input_start_time = request.POST.get('dtp_start_time')
+        input_end_time = request.POST.get('dtp_end_time')
 
-        start_hour = int(start_time[0:2])
-        start_minute = int(start_time[-2:])
-        end_hour = int(end_time[0:2])
-        end_minute = int(end_time[-2:])
+        if input_start_date is "" or input_end_date is "" or input_start_time is "" or input_end_time is "":
+            context['error_msg'] = 'Start date or time cannot be after end date and time. Date and Time cannot be in the past'
+            context['border_color'] = 'red'
+            operations = ["Less Than", "More Than", "Equal to", "Not Equal to", "Less than or Equal to",
+                          "More than or equal to"]
+            freetext_ops = ["Equal to", "Not Equal to"]
+            key_value_types = ["Numeric", "Freetext", "Timestamp"]
+            filter_ops = ["Equal to", "Not Equal to"]
+            filter_key_value_types = ["Numeric", "Timestamp"]
+            detail_names = ["id", "event_name", "created_timestamp", "user_id", "user_type", "device_id",
+                            "device_description", "event_timestamp", "order_id", "ext_transaction_id",
+                            "payment_method_name", "payment_method_ref", "service_id", "service_name", "command_id",
+                            "command_name", "service_command_id", "initiator_user_id", "initiator_user_type",
+                            "initiator_sof_id", "initiator_sof_type_id", "payer_user_id", "payer_user_type",
+                            "payer_user_ref_type", "payer_user_ref_value", "payer_sof_id", "payer_sof_type_id",
+                            "payee_user_id", "payee_user_type", "payee_user_ref_type", "payee_user_ref_value",
+                            "payee_sof_id", "payee_sof_type_id", "currency", "ref_order_id", "amount", "fee", "bonus",
+                            "settlement_amount", "product_name", "product_ref1", "product_ref2", "product_ref3",
+                            "product_ref4", "product_ref5", "state", "status", "notification_status", "is_deleted",
+                            "order_created_timestamp", "order_last_updated_timestamp", "created_client_id",
+                            "executed_client_id"]
 
+            context['operations'] = operations
+            context['key_value_types'] = key_value_types
+            context['detail_names'] = detail_names
+            context['freetext_ops'] = freetext_ops
+            context['filter_ops'] = filter_ops
+            context['filter_key_value_types'] = filter_key_value_types
+            return render(request, self.template_name, context)
+
+        start_hour = int(input_start_time[0:2])
+        start_minute = int(input_start_time[-2:])
+        end_hour = int(input_end_time[0:2])
+        end_minute = int(input_end_time[-2:])
+        start_date = datetime.strptime(input_start_date, "%Y-%m-%d")
+        start_date = start_date.replace(hour=start_hour, minute=start_minute, second=0)
+        end_date = datetime.strptime(input_end_date, "%Y-%m-%d")
+        end_date = end_date.replace(hour=end_hour, minute=end_minute, second=0)
         current_date = datetime.now()
 
-        if str_start_date:
-            start_date = datetime.strptime(str_start_date, "%Y-%m-%d")
-            start_date = start_date.replace(hour=start_hour, minute=start_minute, second=0)
-
-        if str_end_date:
-            end_date = datetime.strptime(str_end_date, "%Y-%m-%d")
-            end_date = end_date.replace(hour=end_hour, minute=end_minute, second=0)
-
-        if start_date is None or start_time is None or end_date is None or end_time is None or start_date > end_date or start_date < current_date or end_date < current_date:
+        if start_date > end_date or start_date < current_date or end_date < current_date:
             context['error_msg'] = 'Start date or time cannot be after end date and time. Date and Time cannot be in the past'
             context['dtp_start_date'] = start_date.strftime('%Y-%m-%d')
             context['dtp_end_date'] = end_date.strftime('%Y-%m-%d')
+            context['border_color'] = 'red'
             return render(request, self.template_name, context)
 
         param_start_date = start_date.strftime('%Y-%m-%dT%H:%M:%SZ')
@@ -113,6 +139,31 @@ class AddMechanic(GroupRequiredMixin, TemplateView, GetHeaderMixin):
             context['error_msg'] = message
             context['dtp_start_date'] = start_date.strftime('%Y-%m-%d')
             context['dtp_end_date'] = end_date.strftime('%Y-%m-%d')
+            operations = ["Less Than", "More Than", "Equal to", "Not Equal to", "Less than or Equal to",
+                          "More than or equal to"]
+            freetext_ops = ["Equal to", "Not Equal to"]
+            key_value_types = ["Numeric", "Freetext", "Timestamp"]
+            filter_ops = ["Equal to", "Not Equal to"]
+            filter_key_value_types = ["Numeric", "Timestamp"]
+            detail_names = ["id", "event_name", "created_timestamp", "user_id", "user_type", "device_id",
+                            "device_description", "event_timestamp", "order_id", "ext_transaction_id",
+                            "payment_method_name", "payment_method_ref", "service_id", "service_name", "command_id",
+                            "command_name", "service_command_id", "initiator_user_id", "initiator_user_type",
+                            "initiator_sof_id", "initiator_sof_type_id", "payer_user_id", "payer_user_type",
+                            "payer_user_ref_type", "payer_user_ref_value", "payer_sof_id", "payer_sof_type_id",
+                            "payee_user_id", "payee_user_type", "payee_user_ref_type", "payee_user_ref_value",
+                            "payee_sof_id", "payee_sof_type_id", "currency", "ref_order_id", "amount", "fee", "bonus",
+                            "settlement_amount", "product_name", "product_ref1", "product_ref2", "product_ref3",
+                            "product_ref4", "product_ref5", "state", "status", "notification_status", "is_deleted",
+                            "order_created_timestamp", "order_last_updated_timestamp", "created_client_id",
+                            "executed_client_id"]
+
+            context['operations']= operations
+            context['key_value_types']= key_value_types
+            context['detail_names']= detail_names
+            context['freetext_ops']= freetext_ops
+            context['filter_ops']= filter_ops
+            context['filter_key_value_types']= filter_key_value_types
             return render(request, self.template_name, context)
 
         self.logger.info('========== Start adding Condition ==========')
