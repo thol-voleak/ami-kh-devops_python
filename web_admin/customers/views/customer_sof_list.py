171e7d6ff4a09cfacf5847a58112da39c4d585f0
Kitchana Thomutok <kitchana.tha@ascendcorp.com>
2017-11-15 18:42:52 +0700

    [EQTR-5302] add pagination for customer > bank sof [reviewed by Keerati]

--- web_admin/customers/views/customer_sof_list.py
+++ web_admin/customers/views/customer_sof_list.py
@@ -6,6 +6,10 @@ from django.conf import settings
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from braces.views import GroupRequiredMixin
+from web_admin.utils import calculate_page_range_from_page_info
+from web_admin.global_constants import UserType
+from web_admin import api_settings, setup_logger, RestFulClient
+from web_admin.api_logger import API_Logger
 
 import logging
 
@@ -16,6 +20,7 @@ class CustomerSOFListView(GroupRequiredMixin, TemplateView, RESTfulMethods):
     group_required = "CAN_VIEW_BANK_SOF_CUSTOMER_PROFILE"
     login_url = 'web:permission_denied'
     raise_exception = False
+    searchcustomerbanksofurl = settings.DOMAIN_NAMES + "report/" + api_settings.API_VERSION + "/banks/sofs"
 
     def check_membership(self, permission):
         self.logger.info(
@@ -33,18 +38,89 @@ class CustomerSOFListView(GroupRequiredMixin, TemplateView, RESTfulMethods):
     def get(self, request, *args, **kwargs):
         self.logger.info('========== Start getting customer sof bank ==========')
 
-        customer_id = int(kwargs.get('customerId'))
+        user_id = int(kwargs.get('customerId'))
 
-        url = settings.DOMAIN_NAMES + "report/" + api_settings.API_VERSION + "/banks/sofs"
-        self.logger.info('API-Path: {};'.format(url))
+        self.logger.info('API-Path: {};'.format(self.searchcustomerbanksofurl))
         param = {
-            "user_id": customer_id
+            "user_id": user_id,
+            "user_type_id": UserType.CUSTOMER.value
         }
-        data, success = self._post_method(api_path=url,
+        data, success = self._post_method(api_path=self.searchcustomerbanksofurl,
                                           func_description="member customer detail",
                                           logger=logger,
                                           params=param)
+        context = {}
+        if success:
+            cards_list = data.get("bank_sofs", [])
+            page = data.get("page", {})
+            self.logger.info("Page: {}".format(page))
+            context.update(
+                {'search_count': page.get('total_elements', 0),
+                 'paginator': page,
+                 'page_range': calculate_page_range_from_page_info(page),
+                 'bank_sof_list': cards_list,
+                 'user_id' : user_id,
+                 }
+            )
+        else:
+            context.update(
+                {'search_count': 0,
+                 'paginator': {},
+                 'user_id': user_id,
+                 'bank_sof_list': [],
+                 }
+            )
 
-        context = {'data': data}
         self.logger.info('========== Finished searching customer sof ==========')
         return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        self.logger.info('========== Start pagination customer bank SOF ==========')
+
+        user_id = request.POST.get('user_id')
+        opening_page_index = request.POST.get('current_page_index')
+
+        body = {}
+        body['paging'] = True
+        body['page_index'] = int(opening_page_index)
+        if user_id is not '' and user_id is not None:
+            body['user_id'] = user_id
+
+        context = {}
+        data, success, status_message = self._get_bank_sof_list(body=body)
+        body['user_type_id'] = UserType.CUSTOMER.value
+        if success:
+            cards_list = data.get("bank_sofs", [])
+            page = data.get("page", {})
+            self.logger.info("Page: {}".format(page))
+            context.update(
+                {'search_count': page.get('total_elements', 0),
+                 'paginator': page,
+                 'page_range': calculate_page_range_from_page_info(page),
+                 'user_id': user_id,
+                 'bank_sof_list': cards_list,
+                 'search_by': body
+                 }
+            )
+        else:
+            context.update(
+                {'search_count': 0,
+                 'paginator': {},
+                 'user_id': user_id,
+                 'bank_sof_list': [],
+                 'search_by': body
+                 }
+            )
+
+        self.logger.info('========== End pagination customer bank SOF ==========')
+        return render(request, self.template_name, context)
+
+    def _get_bank_sof_list(self, body):
+        success, status_code, status_message, data = RestFulClient.post(url=self.searchcustomerbanksofurl,
+                                                                        headers=self._get_headers(),
+                                                                        loggers=self.logger,
+                                                                        params=body)
+        data = data or {}
+        API_Logger.post_logging(loggers=self.logger, params=body, response=data.get('bank_sofs', []),
+                                status_code=status_code, is_getting_list=True)
+        return data, success, status_message
