d39888a623a280bba37136d2588a3319e15006ff
Thang <duythangdoan1994@gmail.com>
2017-11-23 16:00:12 +0700

    EQTR-5543 Update active campaign from function to class Review by Will

--- web_admin/campaign/urls.py
+++ web_admin/campaign/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from .views.list import CampaignList
-from .views.active import active
+from .views.active import ActiveCampaign
 from .views.inactive import inactive
 from .views.create import CreateCampaignView
 from .views.detail import CampaignDetail
@@ -15,7 +15,7 @@ urlpatterns = [
     url(r'^$', login_required(CampaignList.as_view(), login_url='authentications:login'), name="campaign"),
     url(r'^inactive/(?P<campaign_id>[0-9A-Za-z]+)/$', login_required(inactive, login_url='authentications:login'),
         name="inactive-campaign"),
-    url(r'^active/(?P<campaign_id>[0-9A-Za-z]+)/$', login_required(active, login_url='authentications:login'),
+    url(r'^active/(?P<campaign_id>[0-9A-Za-z]+)/$', login_required(ActiveCampaign.as_view(), login_url='authentications:login'),
         name="activate-campaign"),
     url(r'^detail/(?P<campaign_id>[0-9A-Za-z]+)/$', login_required(CampaignDetail.as_view(), login_url='authentications:login'),
         name="campaign_detail"),
--- web_admin/campaign/views/active.py
+++ web_admin/campaign/views/active.py
@@ -2,26 +2,37 @@ from django.conf import settings
 from web_admin import api_settings, setup_logger
 from web_admin import ajax_functions
 import logging
-from web_admin.api_settings import UPDATE_CAMPAIGNS
+from braces.views import GroupRequiredMixin
+from django.views.generic.base import TemplateView
+from .detail import CampaignDetail
+from web_admin.restful_client import RestFulClient
+from web_admin.get_header_mixins import GetHeaderMixin
+from web_admin.api_settings import UPDATE_CAMPAIGNS, GET_MECHANIC_LIST, GET_REWARD_LIST
 from authentications.utils import get_correlation_id_from_username, check_permissions_by_user
 from django.contrib import messages
 import json
 
+logger = logging.getLogger(__name__)
+logging.captureWarnings(True)
 
-def active(request, campaign_id):
-    # if not check_permissions_by_user(request.user, 'CAN_SUSPEND_CLIENTS'):
-    #     return
+class ActiveCampaign(CampaignDetail):
 
-    logger = logging.getLogger(__name__)
-    correlation_id = get_correlation_id_from_username(request.user)
-    logger = setup_logger(request, logger, correlation_id)
-    logger.info('========== Start active campaign ==========')
-    url = settings.DOMAIN_NAMES + UPDATE_CAMPAIGNS.format(bak_rule_id=campaign_id)
-    params = {
-        'is_active': True,
-        'name': request.GET.get("campaign_name"),
-        'description': request.GET.get("campaign_description")
-    }
-    result = ajax_functions._put_method(request, url, "", logger, params)
-    logger.info('========== Finish active campaign ==========')
-    return result
+    logger = logger
+
+    def dispatch(self, request, *args, **kwargs):
+        correlation_id = get_correlation_id_from_username(self.request.user)
+        self.logger = setup_logger(self.request, logger, correlation_id)
+        return super(ActiveCampaign, self).dispatch(request, *args, **kwargs)
+
+    def post(self, request, campaign_id):
+        self.logger.info('========== Start active campaign ==========')
+        url = settings.DOMAIN_NAMES + UPDATE_CAMPAIGNS.format(bak_rule_id=campaign_id)
+        params = {
+            'is_active': True,
+            'name': request.POST.get("campaign_name"),
+            'description': request.POST.get("campaign_description")
+        }
+        result = ajax_functions._put_method(request, url, "", self.logger, params)
+        self.logger.info('========== Finish active campaign ==========')
+        return result
+
