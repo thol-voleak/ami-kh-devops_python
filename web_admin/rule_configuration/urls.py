dc0b2cfbfaa635049c3897fd1c3f9c6acbc2a47a
Thang <duythangdoan1994@gmail.com>
2017-12-05 14:35:13 +0700

    EQTR-5885 Rule List Review by Will

--- web_admin/rule_configuration/urls.py
+++ web_admin/rule_configuration/urls.py
@@ -1,4 +1,11 @@
+from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
+from .views.list import RuleList
+
+
 app_name = 'rule_configuration'
 
 urlpatterns = [
+    url(r'^$', login_required(RuleList.as_view(), login_url='authentications:login'), name="rule_engine"),
 ]
+
--- web_admin/web_admin/api_settings.py
+++ web_admin/web_admin/api_settings.py
@@ -229,3 +229,8 @@ CREATE_COMPARISON = 'api-gateway/rule-engine/'+API_VERSION+'/admin/rules/{rule_i
 CREATE_REWARD =     'api-gateway/rule-engine/'+API_VERSION+'/admin/rules/{rule_id}/mechanics/{mechanic_id}/actions'
 CREATE_LIMITATION = 'api-gateway/rule-engine/'+API_VERSION+'/admin/rules/{rule_id}/mechanics/{mechanic_id}/actions/{action_id}/limits'
 GET_LIMITION_LIST= 'api-gateway/rule-engine/'+API_VERSION+'/admin/rules/{bak_rule_id}/mechanics/{bak_mechanic_id}/actions/{bak_action_id}/limits'
+
+# Rule Engine
+GET_RULE = 'api-gateway/rule-engine/'+API_VERSION+'/admin/rules'
+SEARCH_RULE = 'api-gateway/rule-engine/'+API_VERSION+'/admin/rules/report'
+
