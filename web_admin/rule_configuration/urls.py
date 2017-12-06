21a4cf4516b2fe557787487fa7b0f28e3d34b356
Bui Chi Hoa <chihoa.bui@ascendcorp.com>
2017-12-06 14:08:32 +0700

    [EQTR-5891] [UI] Rule Details - reviewed by Lucian

--- web_admin/rule_configuration/urls.py
+++ web_admin/rule_configuration/urls.py
@@ -4,6 +4,7 @@ from .views.list import RuleList
 from .views.add_condition import AddRuleCondition
 from .views.create_rule import CreateRuleView
 from .views.add_mechanic import AddMechanic
+from .views.detail import RuleDetail
 
 
 app_name = 'rule_configuration'
@@ -15,5 +16,7 @@ urlpatterns = [
     url(r'^(?P<rule_id>[0-9A-Za-z]+)/mechanic$', login_required(AddMechanic.as_view(), login_url='authentications:login'),
         name="add_mechanic"),
     url(r'^create$', login_required(CreateRuleView.as_view(), login_url='authentications:login'),
-        name="create_rule")
+        name="create_rule"),
+    url(r'^detail/(?P<rule_id>[0-9A-Za-z]+)/$', login_required(RuleDetail.as_view(), login_url='authentications:login'),
+        name="rule_detail"),
 ]
