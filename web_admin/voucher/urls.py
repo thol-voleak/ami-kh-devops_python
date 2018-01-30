15981d2742870518f169fe503e55e01d534812f8
Ngo Hoang Lien <lien.ngo@ascendcorp.com>
2018-01-30 15:43:10 +0700

    [EQTR-6957] [UI] Can add new voucher - [reviewed by Lucian]

--- web_admin/voucher/urls.py
+++ web_admin/voucher/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from .views.list import VoucherList
 from .views.detail import VoucherDetail
+from.views.transit import Transit
 
 
 app_name = 'voucher'
@@ -9,4 +10,5 @@ app_name = 'voucher'
 urlpatterns = [
     url(r'^$', login_required(VoucherList.as_view(), login_url='authentications:login'), name="voucher"),
     url(r'^detail/(?P<voucher_id>[0-9A-Za-z]+)/$', login_required(VoucherDetail.as_view(), login_url='authentications:login'), name="voucher_detail"),
+    url(r'^create/$', login_required(Transit.as_view(), login_url='authentications:login'), name="create_new_voucher"),
 ]
---
+++ web_admin/voucher/views/transit.py
@@ -0,0 +1,34 @@
+from authentications.utils import get_correlation_id_from_username, check_permissions_by_user
+from web_admin import setup_logger
+from django.views.generic.base import TemplateView
+from web_admin.get_header_mixins import GetHeaderMixin
+from django.shortcuts import redirect
+import logging
+from braces.views import GroupRequiredMixin
+from django.contrib import messages
+
+logger = logging.getLogger(__name__)
+
+class Transit(GroupRequiredMixin, TemplateView, GetHeaderMixin):
+
+    group_required = "CAN_CREATE_VOUCHER_ACTION"
+    login_url = 'web:permission_denied'
+    logger = logger
+
+    def dispatch(self, request, *args, **kwargs):
+        correlation_id = get_correlation_id_from_username(self.request.user)
+        self.logger = setup_logger(self.request, logger, correlation_id)
+        return super(Transit, self).dispatch(request, *args, **kwargs)
+
+    def check_membership(self, permission):
+        self.logger.info(
+            "Checking permission for [{}] username with [{}] permission".format(self.request.user, permission))
+        return check_permissions_by_user(self.request.user, permission[0])
+
+    def get(self, request, *args, **kwargs):
+        messages.add_message(
+            request,
+            messages.WARNING,
+            'Add New Voucher'
+        )
+        return redirect('balance_adjustment:balance_adjustment_create')
