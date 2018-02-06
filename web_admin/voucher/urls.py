4017b5c82d2ef9238eebe42091a6a5f81ddc078b
Pham Van Nam <nam.pham@ascendcorp.com>
2018-02-06 09:56:17 +0700

    [EQTR-6875] temporary [reviewed by ALex]

--- web_admin/voucher/urls.py
+++ web_admin/voucher/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from .views.list import VoucherList
 from .views.detail import VoucherDetail
-from.views.transit import Transit
+from .views.transit import Transit
+from .views.hold import HoldVoucher, UnholdVoucher
 
 
 app_name = 'voucher'
@@ -11,4 +12,6 @@ urlpatterns = [
     url(r'^$', login_required(VoucherList.as_view(), login_url='authentications:login'), name="voucher"),
     url(r'^detail/(?P<voucher_id>[0-9A-Za-z]+)/$', login_required(VoucherDetail.as_view(), login_url='authentications:login'), name="voucher_detail"),
     url(r'^create/$', login_required(Transit.as_view(), login_url='authentications:login'), name="create_new_voucher"),
+    url(r'^hold/(?P<voucher_id>[0-9A-Za-z]+)/$', login_required(HoldVoucher.as_view(), login_url='authentications:login'), name="hold_voucher"),
+    url(r'^unhold/(?P<voucher_id>[0-9A-Za-z]+)/$', login_required(UnholdVoucher.as_view(), login_url='authentications:login'), name="unhold_voucher")
 ]
