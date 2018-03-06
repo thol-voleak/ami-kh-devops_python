06f8b2d7ce9b57ffaa301717f6e169ef03458620
Bui Chi Hoa <chihoa.bui@ascendcorp.com>
2018-03-06 16:06:22 +0700

    [EQTR-7357] [UI][Fail case] Show error when failed to get agent summary wallet - reviewed by Alex

--- web_admin/balances/views/balance_summary.py
+++ web_admin/balances/views/balance_summary.py
@@ -12,7 +12,6 @@ from django.contrib import messages
 logger = logging.getLogger(__name__)
 from django.shortcuts import render, redirect
 import logging
-import copy
 
 logger = logging.getLogger(__name__)
 
@@ -40,63 +39,78 @@ class BalanceSummary(GroupRequiredMixin, TemplateView, GetHeaderMixin):
         self.logger.info('========== Start render Balance Summary page ==========')
 
         self.logger.info('========== Start getting customer summary ==========')
-        customer_summary = self._get_customer_summary()
+        customer_summary, is_get_customer_summary_success = self._get_customer_summary()
         self.logger.info('========== Finish getting customer summary ==========')
 
-        cus_total_sofs = 0
-        for item in customer_summary['sofs']:
-            cus_total_sofs += item.get('total_sof', 0)
-        customer_summary['total_sof_card'] = cus_total_sofs
+        if is_get_customer_summary_success:
+            cus_total_sofs = 0
+            for item in customer_summary['sofs']:
+                cus_total_sofs += item.get('total_sof', 0)
+            customer_summary['total_sof_card'] = cus_total_sofs
 
         self.logger.info('========== Start getting agent summary ==========')
-        agent_summary = self._get_agent_summary()
+        agent_summary, is_get_agent_summary_success = self._get_agent_summary()
         self.logger.info('========== Finish getting agent summary ==========')
 
         # get agent total soft card and currency balance list
-        sofs = agent_summary['agent_types'][0]['sofs']
-        agent_summary['sofs'] = []
+        if is_get_agent_summary_success:
+            sofs = agent_summary['agent_types'][0]['sofs']
+            agent_summary['sofs'] = []
 
-        for sof in sofs:
-            currency = {'currency': sof['currency'], 'total_balance': 0}
-            agent_summary['sofs'].append(currency)
+            for sof in sofs:
+                currency = {'currency': sof['currency'], 'total_balance': 0}
+                agent_summary['sofs'].append(currency)
 
-        agent_total_sofs = 0
-        for item in agent_summary['agent_types']:
-            for sof in item['sofs']:
-                agent_total_sofs += sof.get('total_sof', 0)
-                for currency in agent_summary['sofs']:
-                    if currency['currency'] == sof['currency']:
-                        currency['total_balance'] += sof['total_balance']
+            agent_total_sofs = 0
+            for item in agent_summary['agent_types']:
+                for sof in item['sofs']:
+                    agent_total_sofs += sof.get('total_sof', 0)
+                    for currency in agent_summary['sofs']:
+                        if currency['currency'] == sof['currency']:
+                            currency['total_balance'] += sof['total_balance']
 
-        agent_summary['agent_total_sofs'] = agent_total_sofs
+            agent_summary['agent_total_sofs'] = agent_total_sofs
 
-        # get agent profile number
-        agent_total_profile = 0
-        for item in agent_summary['agent_types']:
-            agent_total_profile += item.get('total_profile', 0)
+            # get agent profile number
+            agent_total_profile = 0
+            for item in agent_summary['agent_types']:
+                agent_total_profile += item.get('total_profile', 0)
 
-        agent_summary['agent_total_profile'] = agent_total_profile
+            agent_summary['agent_total_profile'] = agent_total_profile
 
-        data = [
-            {'agent_type': 'company',
-             'profile': 'human',
-             'amount': '1000',
-             'fee': '50',
-             'currency': 'vnd'}
-        ]
-
-        context ={
-            'data': data,
-            'customer_summary': customer_summary,
-            'agent_summary': agent_summary,
-        }
+        if is_get_agent_summary_success and is_get_customer_summary_success:
+            context ={
+                'customer_summary': customer_summary,
+                'agent_summary': agent_summary,
+                'is_get_agent_summary_success': 1,
+                'is_get_customer_summary_success': 1
+            }
+        elif is_get_agent_summary_success and not is_get_customer_summary_success:
+            context = {
+                'agent_summary': agent_summary,
+                'is_get_agent_summary_success': 1,
+                'is_get_customer_summary_success': 0
+            }
+        elif not is_get_agent_summary_success and is_get_customer_summary_success:
+            context = {
+                'customer_summary': customer_summary,
+                'is_get_agent_summary_success': 0,
+                'is_get_customer_summary_success': 1
+            }
+        else:
+            context = {
+                'is_get_agent_summary_success': 0,
+                'is_get_customer_summary_success': 0
+            }
 
         self.logger.info('========== Finish render Balance Summary page ==========')
 
         return render(request, self.template_name, context)
 
     def _get_customer_summary(self):
-        is_success, status_code, status_message, data = RestFulClient.post(url=api_settings.GET_CUSTOMER_BALANCE,
+        url = api_settings.GET_CUSTOMER_BALANCE
+        url = 'http://localhost:7357/get_agent_summary'
+        is_success, status_code, status_message, data = RestFulClient.post(url=url,
                                                                            headers=self._get_headers(),
                                                                            loggers=self.logger,
                                                                            params={})
@@ -105,26 +119,18 @@ class BalanceSummary(GroupRequiredMixin, TemplateView, GetHeaderMixin):
                                 status_code=status_code, is_getting_list=False)
 
         if not is_success:
-            messages.add_message(
-                self.request,
-                messages.ERROR,
-                status_message
-            )
             data = {}
-        return data
+        return data, is_success
 
     def _get_agent_summary(self):
-        is_success, status_code, status_message, data = RestFulClient.post(url=api_settings.GET_AGENT_SUMMARY,
+        url = api_settings.GET_AGENT_SUMMARY
+        url = 'http://localhost:7357/get_agent_summary'
+        is_success, status_code, status_message, data = RestFulClient.post(url=url,
                                                                            headers=self._get_headers(),
                                                                            loggers=self.logger,
                                                                            params={})
         API_Logger.post_logging(loggers=self.logger, params={}, response=data,
                                 status_code=status_code, is_getting_list=False)
         if not is_success:
-            messages.add_message(
-                self.request,
-                messages.ERROR,
-                status_message
-            )
             data = {}
-        return data
+        return data, is_success
