c122d18daa21cc69f15b53afe8626d28d7efd720
Thang Doan <thang.doan@ascendcorp.com>
2018-03-13 13:45:12 +0700

    EQTR-7290 Create Categories page Reviewed by Will

--- web_admin/category/urls.py
+++ web_admin/category/urls.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
 from .views.add_category import AddCategory
+from .views.list import CategoryList
 
 app_name = 'category'
 
 urlpatterns = [
+    url(r'^categories/$', login_required(CategoryList.as_view(), login_url='authentications:login'), name="categories"),
     url(r'^create$', login_required(AddCategory.as_view(), login_url='authentications:login'), name="category_add"),
 ]
