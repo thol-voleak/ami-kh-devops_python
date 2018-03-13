0503853f54a1a89bc0e9a8de2849a2d2fd991fb7
Potter Chung <chung.nguyen@ascendcorp.com>
2018-03-13 14:27:25 +0700

    [EQTR-7431] create folder and files for product app, reviewed by Lucian

--- web_admin/web_admin/urls.py
+++ web_admin/web_admin/urls.py
@@ -52,4 +52,5 @@ urlpatterns = [
     url(r'^admin-portal/voucher/', include('voucher.urls')),
     url(r'^admin-portal/report-configuration/', include('report_configuration.urls')),
     url(r'^admin-portal/category/', include('category.urls')),
+    url(r'^admin-portal/product/', include('product.urls')),
 ]
