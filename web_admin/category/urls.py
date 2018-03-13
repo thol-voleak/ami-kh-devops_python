15250020231bffa06d41a8747adc4cab2013541a
Bui Chi Hoa <chihoa.bui@ascendcorp.com>
2018-03-13 11:53:50 +0700

    [EQTR-7294] [UI][Success] Create Add Category page - reviewed by Lucian

--- web_admin/category/urls.py
+++ web_admin/category/urls.py
@@ -5,5 +5,5 @@ from .views.add_category import AddCategory
 app_name = 'category'
 
 urlpatterns = [
-    url(r'^add$', login_required(AddCategory.as_view(), login_url='authentications:login'), name="category_add"),
+    url(r'^create$', login_required(AddCategory.as_view(), login_url='authentications:login'), name="category_add"),
 ]
