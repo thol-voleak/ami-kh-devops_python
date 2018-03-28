fa744b096beecbda8a0d1f0fbc155f9052b8030f
Pham Van Nam <nam.pham@ascendcorp.com>
2018-03-28 14:59:37 +0700

    [EQTR-7556] [UI] Create New Shop Category Page [reviewed by Potter]

--- web_admin/web_admin/api_settings.py
+++ web_admin/web_admin/api_settings.py
@@ -294,6 +294,7 @@ EDIT_SHOP_TYPE = 'api-gateway/agent/' + API_VERSION + '/admin/shop-types/{shop_t
 
 GET_LIST_SHOP_CATEGORIES = 'api-gateway/report/' + API_VERSION + '/shop-categories'
 GET_DETAIL_SHOP_CATEGORIES = 'api-gateway/report/' + API_VERSION + '/shop-categories'
+CREATE_SHOP_CATEGORY = 'api-gateway/agent/' + API_VERSION + '/admin/shop-categories/'
 EDIT_SHOP_CATEGORIES = 'api-gateway/agent/' + API_VERSION + '/admin/shop-categories/{shop_category_id}'
 GET_PRODUCT_AGENT_RELATION = 'api-gateway/report/' + API_VERSION + '/products/agents/relations'
 CREATE_PRODUCT_AGENT_RELATION = 'api-gateway/agent/' + API_VERSION + '/admin/products/agents/relations'
