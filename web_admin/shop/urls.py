7c4304685f81cc3ce0624550e64e9e4c8b824108
Potter Chung <chung.nguyen@ascendcorp.com>
2018-03-28 17:21:34 +0700

    [EQTR-7648] UI design

--- web_admin/shop/urls.py
+++ web_admin/shop/urls.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
 
+from shop.views import ajax
 from .views.create import CreateView
 from .views.delete import DeleteView
 from .views.detail import DetailView
@@ -10,6 +11,7 @@ from .views.list import ListView
 app_name = 'shop'
 
 urlpatterns = [
+url(r'^ajax/agent-detail/(?P<id>[0-9A-Za-z]+)/$', login_required(ajax.agent_detail, login_url='authentications:login'), name="ajax_agent_detail"),
     url(r'^$', login_required(ListView.as_view(), login_url='authentications:login'), name="shop_list"),
     url(r'^create/$', login_required(CreateView.as_view(), login_url='authentications:login'), name="shop_create"),
     url(r'^(?P<id>[0-9A-Za-z]+)/edit/$', login_required(EditView.as_view(), login_url='authentications:login'), name="shop_edit"),
