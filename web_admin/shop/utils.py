712d59aa3937d189d86c88ec8b4af0f8b619fd0c
Tran Tuan Anh <anh.tuan@ascendcorp.com>
2018-03-28 17:33:22 +0700

    [EQTR-7649] Add common func - Reviewed by Leo

--- web_admin/shop/utils.py
+++ web_admin/shop/utils.py
@@ -1,32 +1,37 @@
-def get_all_shop_type():
-    # TODO
-    list = [
-        {
-            "id": 1,
-            "name": "1"
-        },
-        {
-            "id": 2,
-            "name": "2"
-        }
-    ]
+from web_admin.restful_client import RestFulClient
+from web_admin.api_logger import API_Logger
+from web_admin import api_settings
 
-    return list
 
-def get_all_shop_category():
-    # TODO
-    list = [
-        {
-            "id": 1,
-            "name": "1"
-        },
-        {
-            "id": 2,
-            "name": "2"
-        }
-    ]
+def get_all_shop_type(self):
+    api_path = api_settings.GET_LIST_SHOP_TYPE
+    success, status_code, status_message, data = RestFulClient.post(
+        url=api_path,
+        headers=self._get_headers(),
+        loggers=self.logger)
 
-    return list
+    data = data or {}
+    API_Logger.post_logging(loggers=self.logger,
+                            response=data.get('shop_types', []),
+                            status_code=status_code,
+                            is_getting_list=True,
+                            params={'paging': False, 'is_deleted': False})
+    return data.get('shop_types', [])
+
+def get_all_shop_category(self):
+    api_path = api_settings.GET_LIST_SHOP_CATEGORIES
+    success, status_code, status_message, data = RestFulClient.post(
+        url=api_path,
+        headers=self._get_headers(),
+        loggers=self.logger)
+
+    data = data or {}
+    API_Logger.post_logging(loggers=self.logger,
+                            response=data.get('shop_categories', []),
+                            status_code=status_code,
+                            is_getting_list=True,
+                            params={'paging': False, 'is_deleted': False})
+    return data.get('shop_categories', [])
 
 def get_system_country():
     # TODO
