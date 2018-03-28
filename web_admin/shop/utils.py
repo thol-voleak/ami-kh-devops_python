545b7f526008c745c3a66c071960f4ea1cae2ba0
Potter Chung <chung.nguyen@ascendcorp.com>
2018-03-28 16:02:01 +0700

    [EQTR-7649] design, reviewed by Lucian

---
+++ web_admin/shop/utils.py
@@ -0,0 +1,33 @@
+def get_all_shop_type():
+    # TODO
+    list = [
+        {
+            "id": 1,
+            "name": "1"
+        },
+        {
+            "id": 2,
+            "name": "2"
+        }
+    ]
+
+    return list
+
+def get_all_shop_category():
+    # TODO
+    list = [
+        {
+            "id": 1,
+            "name": "1"
+        },
+        {
+            "id": 2,
+            "name": "2"
+        }
+    ]
+
+    return list
+
+def get_system_country():
+    # TODO
+    return "VN"
