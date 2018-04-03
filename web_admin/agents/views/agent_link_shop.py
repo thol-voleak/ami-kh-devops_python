522f500f3846fa2c7ba8a243f8cb76a0b2a5d22c
Tran Tuan Anh <anh.tuan@ascendcorp.com>
2018-04-03 15:45:55 +0700

    [EQTR-7549] check permission CAN_LINK_SHOP - Reviewed by Potter

--- web_admin/agents/views/agent_link_shop.py
+++ web_admin/agents/views/agent_link_shop.py
@@ -18,10 +18,17 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class AgentLinkShop(TemplateView, GetHeaderMixin):
+class AgentLinkShop(GroupRequiredMixin, TemplateView, GetHeaderMixin):
     template_name = 'agents/agent_link_shop.html'
+    group_required = 'CAN_LINK_SHOP'
+    login_url = 'web:permission_denied'
     raise_exception = False
     logger = logger
+
+    def check_membership(self, permission):
+        self.logger.info(
+            "Checking permission for [{}] username with [{}] permission".format(self.request.user, permission))
+        return check_permissions_by_user(self.request.user, permission[0])
 
     def dispatch(self, request, *args, **kwargs):
         correlation_id = get_correlation_id_from_username(self.request.user)
