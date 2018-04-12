14bc0baaa377f86d271f75028c20de71d9b6218f
Bui Chi Hoa <chihoa.bui@ascendcorp.com>
2018-04-12 10:02:18 +0700

    [EQTR-7910] - update log - reviewed by Potter

--- web_admin/channel_gateway/service/views/delete.py
+++ web_admin/channel_gateway/service/views/delete.py
@@ -9,6 +9,7 @@ from authentications.apps import InvalidAccessToken
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from web_admin.api_logger import API_Logger
+from web_admin.restful_helper import RestfulHelper
 import logging
 
 logger = logging.getLogger(__name__)
@@ -27,22 +28,14 @@ class DeleteView(TemplateView, GetHeaderMixin):
         services_id = int(kwargs['id'])
         services = self.get_service_detail(services_id)
         context = {
-            "form": services["services"][0]
+            "form": services
         }
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
         services_id = int(kwargs['id'])
         api_path = api_settings.DELETE_CHANNEL_SERVICE.format(service_id=services_id)
-        self.logger.info('========== Start delete service  ==========')
-        success, status_code, status_message = RestFulClient.delete(
-            url=api_path,
-            headers=self._get_headers(),
-            loggers=self.logger)
-
-        API_Logger.delete_logging(loggers=self.logger,
-                                  status_code=status_code)
-        self.logger.info('========== Finish delete service  ==========')
+        success, status_code, status_message, data = RestfulHelper.send("DELETE", api_path, {}, self.request, "delete service")
 
         if success:
             messages.add_message(
@@ -55,24 +48,16 @@ class DeleteView(TemplateView, GetHeaderMixin):
             messages.add_message(request, messages.ERROR, status_message)
             services = self.get_service_detail(services_id)
             context = {
-                "form": services["services"][0]
+                "form": services
             }
             return render(request, self.template_name, context)
 
     def get_service_detail(self, services_id):
         url = api_settings.GET_CHANNEL_SERVICE
-        self.logger.info('========== Start get service detail ==========')
         params = {
             "id": services_id
         }
-        is_success, status_code, status_message, data = RestFulClient.post(url,
-                                                                           headers=self._get_headers(),
-                                                                           params=params, loggers=self.logger)
+        is_success, status_code, status_message, data = RestfulHelper.send("POST", url, params, self.request, "get service detail")
         if data is None:
-            data = {}
-            data["services"] = []
-
-        API_Logger.post_logging(loggers=self.logger, params=params, response=data.get('services', []),
-                                status_code=status_code, is_getting_list=False)
-        self.logger.info('========== Finish get service detail ==========')
-        return data
+            return None
+        return data["services"][0]
