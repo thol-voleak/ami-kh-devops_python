04af55f253253307b3912b17971a9471c6bb3097
Tran Tuan Anh <anh.tuan@ascendcorp.com>
2018-04-13 14:27:03 +0700

    [EQTR-7923] Check permission CAN_VIEW_GW_SERVICE - Reviewed by Potter

--- web_admin/channel_gateway/service/views/detail.py
+++ web_admin/channel_gateway/service/views/detail.py
@@ -1,17 +1,9 @@
-from authentications.utils import get_correlation_id_from_username, check_permissions_by_user
-from braces.views import GroupRequiredMixin
-from web_admin import setup_logger, api_settings
-from web_admin.restful_client import RestFulClient
+from web_admin import api_settings
 from django.views.generic.base import TemplateView
 from web_admin.get_header_mixins import GetHeaderMixin
-from django.conf import settings
-from authentications.apps import InvalidAccessToken
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from web_admin.api_logger import API_Logger
-from django.http import JsonResponse
+from django.shortcuts import render
 import logging
-
+from web_admin.utils import check_permissions, build_logger
 from web_admin.restful_helper import RestfulHelper
 
 logger = logging.getLogger(__name__)
@@ -23,8 +15,8 @@ class DetailView(TemplateView, GetHeaderMixin):
     logger = logger
 
     def dispatch(self, request, *args, **kwargs):
-        correlation_id = get_correlation_id_from_username(self.request.user)
-        self.logger = setup_logger(self.request, logger, correlation_id)
+        check_permissions(request, "CAN_VIEW_GW_SERVICE")
+        self.logger = build_logger(request, __name__)
         return super(DetailView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
@@ -41,7 +33,7 @@ class DetailView(TemplateView, GetHeaderMixin):
             "id": services_id
         }
         is_success, status_code, status_message, data = RestfulHelper.send("POST", url, params, self.request, "get service detail")
-        if data is None:
-            return None;
+        if not data:
+            return {}
         return data["services"][0]
         
