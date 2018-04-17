d5cc56709bc998e67acc41d576bd3da48f34b515
Tran Van Phong <phong.tran@ascendcorp.com>
2018-04-17 15:25:48 +0700

    EQTR-8067 - As a system user, If i click on the approve button, all my selected balance adjustments will be sent off to be approved, reviewed by Potter

--- web_admin/balance_adjustment/urls.py
+++ web_admin/balance_adjustment/urls.py
@@ -1,4 +1,4 @@
-from balance_adjustment.views.list import BalanceAdjustmentListView
+from balance_adjustment.views.list import (BalanceAdjustmentListView, BalanceAdjustmentListActionView)
 from balance_adjustment.views.create import BalanceAdjustmentCreateView
 from balance_adjustment.views.detail import BalanceAdjustmentDetailView
 
@@ -13,4 +13,6 @@ urlpatterns = [
     url(r'^execute/$', login_required(BalanceAdjustmentCreateView.as_view(), login_url='authentications:login'), name="balance_adjustment_create"),
     url(r'^details/(?P<ReferenceId>[0-9A-Za-z]+)/$', login_required(BalanceAdjustmentDetailView.as_view(), login_url='authentications:login'), name="balance_adjustment_detail"),
     url(r'^approve/(?P<OrderId>[0-9A-Za-z]+)/$', login_required(BalanceAdjustmentDetailView.as_view(), login_url='authentications:login'), name="balance_adjustment_approve"),
+
+    url(r'^process_multi_balance_adjustment/$', login_required(BalanceAdjustmentListActionView.as_view(), login_url='authentications:login'), name="process_multi_balance_adjustments"),
 ]
