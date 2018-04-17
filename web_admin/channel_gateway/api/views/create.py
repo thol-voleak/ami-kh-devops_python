6673cf58b7e12731c67307de15b75d4f45dfaefa
Tran Tuan Anh <anh.tuan@ascendcorp.com>
2018-04-17 09:38:16 +0700

    [EQTR-7927] Check permission CAN_CREATE_GW_API - Reviewed by Potter

--- web_admin/channel_gateway/api/views/create.py
+++ web_admin/channel_gateway/api/views/create.py
@@ -1,4 +1,4 @@
-from authentications.utils import get_correlation_id_from_username, check_permissions_by_user
+from authentications.utils import get_correlation_id_from_username
 from web_admin import setup_logger, api_settings
 from web_admin.restful_helper import RestfulHelper
 from django.views.generic.base import TemplateView
@@ -6,24 +6,17 @@ from web_admin.get_header_mixins import GetHeaderMixin
 from django.shortcuts import render, redirect
 from django.contrib import messages
 import logging
+from web_admin.utils import build_logger, check_permissions
 from channel_gateway.api.utils  import get_service_list
-
-logger = logging.getLogger(__name__)
 
 
 class CreateView(TemplateView, GetHeaderMixin):
     template_name = "channel-gateway-api/create.html"
-    logger = logger
-
-    def check_membership(self, permission):
-        self.logger.info(
-            "Checking permission for [{}] username with [{}] permission".format(
-                self.request.user, permission))
-        return check_permissions_by_user(self.request.user, permission[0])
+    logger = logging.getLogger(__name__)
 
     def dispatch(self, request, *args, **kwargs):
-        correlation_id = get_correlation_id_from_username(self.request.user)
-        self.logger = setup_logger(self.request, logger, correlation_id)
+        check_permissions(request, 'CAN_CREATE_GW_API')
+        self.logger = build_logger(request, __name__)
         return super(CreateView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
