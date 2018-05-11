fa563d334b067c6c147a074ddd47fedcfaa2bf49
Trinh Viet Dung <dung.trinh@ascendcorp.com>
2018-05-11 11:36:14 +0700

    [EQTR-8479] - [UI][Add service tier] Add more fields related to A-O to Add service tier page, reviewed by Frank

--- web_admin/services/views/tier/add.py
+++ web_admin/services/views/tier/add.py
@@ -6,6 +6,7 @@ from web_admin import api_settings, setup_logger
 from web_admin.restful_methods import RESTfulMethods
 from authentications.utils import get_correlation_id_from_username
 from django.contrib import messages
+from services.views.tier_levels.utils import get_label_levels
 
 logger = logging.getLogger(__name__)
 
@@ -36,8 +37,30 @@ class AddView(TemplateView, RESTfulMethods):
         command_name, status4 = self._get_command_name(command_id)
         fee_types, status5 = self._get_fee_types()
         bonus_types, status6 = self._get_bonus_types()
+        tier_amount_froms, status7 = self._get_tier_amount_froms()
+        self.logger.info('========== Finish get tier amount froms ==========')
+        payment_decimals, status8 = self._get_payment_decimal()
+        self.logger.info('========== Finish get payment decimal ==========')
 
-        if status1 and status2 and status3 and status4 and status5 and status6:
+        payment_decimal = payment_decimals['value']
+
+        a_label = self.get_label_detail('A')
+        b_label = self.get_label_detail('B')
+        c_label = self.get_label_detail('C')
+        d_label = self.get_label_detail('D')
+        e_label = self.get_label_detail('E')
+        f_label = self.get_label_detail('F')
+        g_label = self.get_label_detail('G')
+        h_label = self.get_label_detail('H')
+        i_label = self.get_label_detail('I')
+        j_label = self.get_label_detail('J')
+        k_label = self.get_label_detail('K')
+        l_label = self.get_label_detail('L')
+        m_label = self.get_label_detail('M')
+        n_label = self.get_label_detail('N')
+        o_label = self.get_label_detail('O')
+
+        if status1 and status2 and status3 and status4 and status5 and status6 and status7 and status8:
             context.update({
                 'conditions': tier_conditions,
                 'fee_types': fee_types,
@@ -46,6 +69,23 @@ class AddView(TemplateView, RESTfulMethods):
                 'service_name': service_detail.get('service_name', 'unknown'),
                 'command_name': command_name,
                 'decimal': int(decimal),
+                'tier_amount_froms': tier_amount_froms,
+                'payment_decimal': payment_decimal,
+                'a_label': a_label,
+                'b_label': b_label,
+                'c_label': c_label,
+                'd_label': d_label,
+                'e_label': e_label,
+                'f_label': f_label,
+                'g_label': g_label,
+                'h_label': h_label,
+                'i_label': i_label,
+                'j_label': j_label,
+                'k_label': k_label,
+                'l_label': l_label,
+                'm_label': m_label,
+                'n_label': n_label,
+                'o_label': o_label,
             })
         self.logger.info('========== Finish loading Adding Tier page ==========')
         return render(request, self.template_name, context)
@@ -64,6 +104,51 @@ class AddView(TemplateView, RESTfulMethods):
         condition = request.POST.get('condition', '')
         bonus_type = request.POST.get('bonus_type', '')
         bonus_amount = request.POST.get('bonus_amount', '')
+        a = request.POST.get('a')
+        a_from = request.POST.get('a_from')
+        a_amount = request.POST.get('a_amount')
+        b = request.POST.get('b')
+        b_from = request.POST.get('b_from')
+        b_amount = request.POST.get('b_amount')
+        c = request.POST.get('c')
+        c_from = request.POST.get('c_from')
+        c_amount = request.POST.get('c_amount')
+        d = request.POST.get('d')
+        d_from = request.POST.get('d_from')
+        d_amount = request.POST.get('d_amount')
+        e = request.POST.get('e')
+        e_from = request.POST.get('e_from')
+        e_amount = request.POST.get('e_amount')
+        f = request.POST.get('f')
+        f_from = request.POST.get('f_from')
+        f_amount = request.POST.get('f_amount')
+        g = request.POST.get('g')
+        g_from = request.POST.get('g_from')
+        g_amount = request.POST.get('g_amount')
+        h = request.POST.get('h')
+        h_from = request.POST.get('h_from')
+        h_amount = request.POST.get('h_amount')
+        i = request.POST.get('i')
+        i_from = request.POST.get('i_from')
+        i_amount = request.POST.get('i_amount')
+        j = request.POST.get('j')
+        j_from = request.POST.get('j_from')
+        j_amount = request.POST.get('j_amount')
+        k = request.POST.get('k')
+        k_from = request.POST.get('k_from')
+        k_amount = request.POST.get('k_amount')
+        l = request.POST.get('l')
+        l_from = request.POST.get('l_from')
+        l_amount = request.POST.get('l_amount')
+        m = request.POST.get('m')
+        m_from = request.POST.get('m_from')
+        m_amount = request.POST.get('m_amount')
+        n = request.POST.get('n')
+        n_from = request.POST.get('n_from')
+        n_amount = request.POST.get('n_amount')
+        o = request.POST.get('o')
+        o_from = request.POST.get('o_from')
+        o_amount = request.POST.get('o_amount')
 
         if condition_amount:
             condition_amount = condition_amount.replace(',', '')
@@ -81,8 +166,86 @@ class AddView(TemplateView, RESTfulMethods):
             "fee_amount": fee_amount,
             "bonus_type": bonus_type,
             "bonus_amount": bonus_amount,
-            "settlement_type": settlement_type
+            "settlement_type": settlement_type,
+            "a": a,
+            "a_from": a_from,
+            "a_amount": a_amount,
+            "b": b,
+            "b_from": b_from,
+            "b_amount": b_amount,
+            "c": c,
+            "c_from": c_from,
+            "c_amount": c_amount,
+            "d": d,
+            "d_from": d_from,
+            "d_amount": d_amount,
+            "e": e,
+            "e_from": e_from,
+            "e_amount": e_amount,
+            "f": f,
+            "f_from": f_from,
+            "f_amount": f_amount,
+            "g": g,
+            "g_from": g_from,
+            "g_amount": g_amount,
+            "h": h,
+            "h_from": h_from,
+            "h_amount": h_amount,
+            "i": i,
+            "i_from": i_from,
+            "i_amount": i_amount,
+            "j": j,
+            "j_from": j_from,
+            "j_amount": j_amount,
+            "k": k,
+            "k_from": k_from,
+            "k_amount": k_amount,
+            "l": l,
+            "l_from": l_from,
+            "l_amount": l_amount,
+            "m": m,
+            "m_from": m_from,
+            "m_amount": m_amount,
+            "n": n,
+            "n_from": n_from,
+            "n_amount": n_amount,
+            "o": o,
+            "o_from": o_from,
+            "o_amount": o_amount
         }
+
+        if params['bonus_type'] == "Flat value" or params['bonus_type'] == "":
+            params['amount_type'] = ''
+        if params['a'] == "Flat value" or params['a'] == "":
+            params['a_from'] = None
+        if params['b'] == "Flat value" or params['b'] == "":
+            params['b_from'] = None
+        if params['c'] == "Flat value" or params['c'] == "":
+            params['c_from'] = None
+        if params['d'] == "Flat value" or params['d'] == "":
+            params['d_from'] = None
+        if params['e'] == "Flat value" or params['e'] == "":
+            params['e_from'] = None
+        if params['f'] == "Flat value" or params['f'] == "":
+            params['f_from'] = None
+        if params['g'] == "Flat value" or params['g'] == "":
+            params['g_from'] = None
+        if params['h'] == "Flat value" or params['h'] == "":
+            params['h_from'] = None
+        if params['i'] == "Flat value" or params['i'] == "":
+            params['i_from'] = None
+        if params['j'] == "Flat value" or params['j'] == "":
+            params['j_from'] = None
+        if params['k'] == "Flat value" or params['k'] == "":
+            params['k_from'] = None
+        if params['l'] == "Flat value" or params['l'] == "":
+            params['l_from'] = None
+        if params['m'] == "Flat value" or params['m'] == "":
+            params['m_from'] = None
+        if params['n'] == "Flat value" or params['n'] == "":
+            params['n_from'] = None
+        if params['o'] == "Flat value" or params['o'] == "":
+            params['o_from'] = None
 
         self.logger.info("Create tire for service command id [{}] with param [{}]".format(service_command_id, params))
 
@@ -247,3 +410,24 @@ class AddView(TemplateView, RESTfulMethods):
         except Exception :
             return False
         return True
+
+    def _get_tier_amount_froms(self):
+        self.logger.info('========== Start get tier amount froms ==========')
+        return self._get_method(api_settings.TIER_AMOUNT_FROMS,
+                                func_description="Tier amount froms",
+                                logger=logger)
+
+    def _get_payment_decimal(self):
+        self.logger.info('========== Start get payment decimal ==========')
+        return self._get_method(api_settings.PAYMENT_DECIMAL,
+                                func_description="Payment decimal",
+                                logger=logger)
+
+    def get_label_detail(self, lvl_name):
+        label_levels = self.request.session.get('tier_levels')
+        if not label_levels:
+            label_levels = get_label_levels(self.request)
+
+        for lvl in label_levels:
+            if lvl.get('name') == lvl_name:
+                return lvl.get('label')
