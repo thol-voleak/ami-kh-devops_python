be8337b6cc6919eca11b69330f98eb0114709689
Tran Tuan Anh <anh.tuan@ascendcorp.com>
2018-06-11 16:59:49 +0700

    EQTR-9159 - Pagination for service page - review by Pine

--- web_admin/services/views/services_list.py
+++ web_admin/services/views/services_list.py
@@ -3,6 +3,7 @@ from web_admin.api_settings import SEARCH_SERVICE, SERVICE_GROUP_LIST_URL
 from django.views.generic.base import TemplateView
 from web_admin.utils import build_logger, check_permissions
 from web_admin.restful_helper import RestfulHelper
+from web_admin.utils import calculate_page_range_from_page_info
 from django.shortcuts import render
 import logging
 
@@ -24,7 +25,7 @@ class ListView(TemplateView):
         return super(ListView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
-        params = {'paging': False}
+        params = {'paging': True, 'page_index': 1}
         context = {}
 
         name = request.GET.get('name')
@@ -32,6 +33,7 @@ class ListView(TemplateView):
         currency = request.GET.get('currency')
         # group = request.GET.get('group')
         status = request.GET.get('status')
+        opening_page_index = request.GET.get('current_page_index')
 
         if id:
             context['id'] = id
@@ -57,7 +59,14 @@ class ListView(TemplateView):
         if status in ['0', '1']:
             params['status'] = int(status)
 
-        service_list = self.get_services_list(params)
+        if opening_page_index:
+            params['page_index'] = int(opening_page_index)
+            context['current_page_index'] = int(opening_page_index)
+
+        success, data = self.get_services_list(params)
+        service_list = data.get('services') if success else []
+        page = data.get("page", {})
+        context['search_count'] = page.get('total_elements', 0)
         service_group_list = self.get_service_group_list()
         service_groups = []
         for i in service_list:
@@ -73,6 +82,8 @@ class ListView(TemplateView):
         permissions['CAN_DELETE_SERVICE'] = check_permissions_by_user(self.request.user, 'CAN_DELETE_SERVICE')
         permissions['CAN_ADD_SERVICE'] = check_permissions_by_user(self.request.user, 'CAN_ADD_SERVICE')
 
+        context['paginator'] = page
+        context['page_range'] = calculate_page_range_from_page_info(page)
         context['data'] = service_list
         context['service_groups'] = service_groups
         context['permissions'] = permissions
@@ -81,7 +92,7 @@ class ListView(TemplateView):
     def get_services_list(self, params):
         url = SEARCH_SERVICE
         success, status_code, status_message, data = RestfulHelper.send("POST", url, params, self.request, "searching service", "data.services")
-        return data.get('services') if success else []
+        return success, data
 
     def get_service_group_list(self):
         url = SERVICE_GROUP_LIST_URL
