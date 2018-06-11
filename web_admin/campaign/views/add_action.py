3b6353aa443758e3512669b14d6dc78f707da683
Le Huy Long <long.le@ascendcorp.com>
2018-06-11 16:02:49 +0700

    [EQTR-9092] - [UI] User want to rename 'Link Bank' to 'Create SOF' in 'Add Mechanic' page - [Reviewed by Thinh]

--- web_admin/campaign/views/add_action.py
+++ web_admin/campaign/views/add_action.py
@@ -262,7 +262,7 @@ class AddAction(TemplateView, GetHeaderMixin):
     def _filter_trigger(self, data):
         filtered = [v for v in data if
                     ((v.term == 'register_customer') or (v.term == 'executed_order') or (v.term == 'login'))]
-        link_bank = {'term': 'created_sof', 'description': 'Link Bank'}
+        link_bank = {'term': 'created_sof', 'description': 'Create SOF'}
         created_order = {'term': 'create_order', 'description': 'Create Order'}
         limit_reached = {'term': 'limit_reached', 'description': 'Limit Reached'}
         filtered.extend([link_bank, created_order, limit_reached])
