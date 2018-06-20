c661450f2f3c782d4129cf2e9165a9f68aa8b3bd
Bui Ngoc Thanh <thanh.bui1@ascendcorp.com>
2018-06-20 13:47:54 +0700

    [EQTR-9357] [UI][Success][F1] As a system user, I'd like to see voucher details pop-up before refund process - (Reviewed by AD)

--- web_admin/voucher/urls.py
+++ web_admin/voucher/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
+
+from voucher.views.refund import VoucherRefundView
 from .views.list import VoucherList
 from .views.detail import VoucherDetail
 from .views.transit import Transit
@@ -13,5 +15,6 @@ urlpatterns = [
     url(r'^detail/(?P<voucher_id>[0-9A-Za-z]+)/$', login_required(VoucherDetail.as_view(), login_url='authentications:login'), name="voucher_detail"),
     url(r'^create/$', login_required(Transit.as_view(), login_url='authentications:login'), name="create_new_voucher"),
     url(r'^hold$', login_required(HoldVoucher.as_view(), login_url='authentications:login'), name="hold_voucher"),
-    url(r'^unhold$', login_required(UnholdVoucher.as_view(), login_url='authentications:login'), name="unhold_voucher")
+    url(r'^unhold$', login_required(UnholdVoucher.as_view(), login_url='authentications:login'), name="unhold_voucher"),
+    url(r'^refund', login_required(VoucherRefundView.as_view(), login_url='authentications:login'), name="refund")
 ]
