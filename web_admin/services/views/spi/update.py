e6306ef553d790c7a0b46746d9685262264c4f33
Tran Tuan Anh <anh.tuan@ascendcorp.com>
2018-06-26 16:28:46 +0700

    EQTR-9417 - handle failecase on edit spi - review by Eric

--- web_admin/services/views/spi/update.py
+++ web_admin/services/views/spi/update.py
@@ -7,7 +7,7 @@ from web_admin.api_logger import API_Logger
 from django.contrib import messages
 from django.http import Http404
 from django.views.generic.base import TemplateView
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 import logging
 
@@ -51,6 +51,9 @@ class SPIUpdate(TemplateView, GetHeaderMixin):
         max_retry = request.POST.get('max_retry', 0)
         retry_delay = request.POST.get('retry_delay', 0)
         expire_in_minute = request.POST.get('expire_in_minute', 0)
+        created_timestamp = request.POST.get('created_timestamp')
+        last_updated_timestamp = request.POST.get('last_updated_timestamp')
+
 
         if spi_url_option == 'internal':
             spi_url = spi_url_value_internal
@@ -78,18 +81,40 @@ class SPIUpdate(TemplateView, GetHeaderMixin):
                 message_text = 'Updated data successfully'
                 self.logger.info('========== Finish updated SPI url ==========')
             elif status_code in ["access_token_expire", 'authentication_fail', 'invalid_access_token']:
-                self.logger.info("{}".format(status_message))
                 self.logger.info('========== Finish updated SPI url ==========')
-                raise InvalidAccessToken(status_message)
+                raise InvalidAccessToken(message)
             else:
-                message_level = messages.ERROR
-                message_text = message
                 self.logger.info('========== Finish updated SPI url ==========')
-                return redirect('services:spi_update',
-                                service_command_id=service_command_id,
-                                command_id=command_id,
-                                service_id=service_id,
-                                spiUrlId=spi_url_id)
+                context = super(SPIUpdate, self).get_context_data(**kwargs)
+                context.update({'data':{
+                    'spi_url_id': spi_url_id,
+                    'spi_url_type': spi_type,
+                    'spi_url_call_method': spi_url_call_method,
+                    'read_timeout': read_timeout,
+                    'max_retry': max_retry,
+                    'retry_delay_millisecond': retry_delay,
+                    'expire_in_minute': expire_in_minute,
+                    'created_timestamp': created_timestamp,
+                    'last_updated_timestamp': last_updated_timestamp,
+                    'internal_url': spi_url_value_internal,
+                    'external_url': spi_url_value_external,
+                }})
+                spi_types = self.get_spi_types()
+                data_spi_call_method = self.get_call_method()
+                context['internal'] = True if spi_url_option == 'internal' else False
+                context['external'] = True if spi_url_option == 'external' else False
+                context['spi_types'] = spi_types
+                context['data_spi_call_method'] = data_spi_call_method
+                context['service'] = service_id
+                context['command'] = command_id
+                context['service_command'] = service_command_id
+
+                messages.add_message(
+                    request,
+                    messages.ERROR,
+                    message
+                )
+                return render(request, self.template_name, context=context)
         else:
             message_level = messages.ERROR
             message_text = "SPI url cannot be empty."
@@ -112,6 +137,8 @@ class SPIUpdate(TemplateView, GetHeaderMixin):
 
     def get_context_data(self, **kwargs):
         context = super(SPIUpdate, self).get_context_data(**kwargs)
+        service_id = kwargs.get('service_id')
+        command_id = kwargs.get('command_id')
         service_command_id = kwargs.get('service_command_id')
         spi_url_id = kwargs.get('spiUrlId')
 
@@ -137,6 +164,9 @@ class SPIUpdate(TemplateView, GetHeaderMixin):
             context['spi_types'] = spi_types
             context['data_spi_call_method'] = data_spi_call_method
             context['add_spi_url_msg'] = self.request.session.pop('add_spi_url_msg', None)
+            context['service'] = service_id
+            context['command'] = command_id
+            context['service_command'] = service_command_id
         elif (status_code == "access_token_expire") or (status_code == 'authentication_fail') or (
                     status_code == 'invalid_access_token'):
             self.logger.info("{}".format(data))
