597e77bade10b1cf58274f4086cdeeb6c442165e
Anh Dao <anh.dao@ascendcorp.com>
2018-07-05 16:10:17 +0700

    [EQTR-10409][UI][Cash-SOF transaction][Update]Display pop-up to show list of user that have phone_number matching with phone search - [Reviewed by Mr.Rey]

--- web_admin/cash_sofs/urls.py
+++ web_admin/cash_sofs/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import url
 
 from .views.cash_sof_list import CashSOFView
 from .views.cash_transaction_list import CashTransactionView
+from .views.user_list import UserList
 
 app_name = 'cash_sofs'
 
 urlpatterns = [
     url(r'^sofs/$', login_required(CashSOFView.as_view(), login_url='authentications:login'), name="cash_sof"),
     url(r'^transaction/$', login_required(CashTransactionView.as_view(), login_url='authentications:login'), name="cash_transaction"),
+    url(r'^users/$', login_required(UserList.as_view(), login_url='authentications:login'), name="search_user")
 ]
