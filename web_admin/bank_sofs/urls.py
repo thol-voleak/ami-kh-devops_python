86c86bbacb034b795dde906ecfebd07cba9b5496
Vu Quoc Dat <dat.vu@ascendcorp.com>
2018-07-17 16:24:37 +0700

    EQTR-10344 [UI][Bank-SOF transaction] Auto fill user_id and user_type searching criteria when searching profile by phone number return 1 entry - review by Rey

--- web_admin/bank_sofs/urls.py
+++ web_admin/bank_sofs/urls.py
@@ -3,11 +3,12 @@ from bank_sofs.views.sof.bank_sof_transaction import BankSOFTransaction
 
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-
+from bank_sofs.views.sof.user_list import UserList
 
 app_name = 'bank_sofs'
 
 urlpatterns = [
     url(r'^sofs/$', login_required(BankSOFView.as_view(), login_url='authentications:login'), name="bank_sof"),
     url(r'^sofs/transaction/$', login_required(BankSOFTransaction.as_view(), login_url='authentications:login'), name="bank_sofs_transaction"),
+    url(r'^users/$', login_required(UserList.as_view(), login_url='authentications:login'), name="search_user")
 ]
