d7d8ee6cd866f11cb640eca7b81fbc8422d4bed7
Christopher Grigg <christopher.gri@ip-172-23-249-87.ap-southeast-1.compute.internal>
2018-07-23 18:37:39 +0700

    Add for create by display name

--- web_admin/services/views/create.py
+++ web_admin/services/views/create.py
@@ -52,12 +52,14 @@ class CreateView(GroupRequiredMixin, TemplateView, RESTfulMethods):
         description = request.POST.get('description')
         clone_from = request.POST.get('clone_from')
         clone_service_name = request.POST.get('service_name_hidden')
+        display_name = request.POST.get('display_name')
 
         body = {
             'service_group_id': service_group_id,
             'service_name': service_name,
             'currency': currency,
             'description': description,
+            'display_name': display_name
         }
 
         if clone_from.isdigit():
